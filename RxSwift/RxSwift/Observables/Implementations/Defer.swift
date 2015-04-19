f20b9cc1435b07629a56201a484e7c666c52b26e
Krunoslav Zaher <kzaher@Krunoslavs-MacBook-Pro.local>
2015-04-19 09:49:26 +0200

    Adds defer implementation.

--- RxSwift/RxSwift/Observables/Implementations/Defer.swift
+++ RxSwift/RxSwift/Observables/Implementations/Defer.swift
@@ -7,3 +7,60 @@
 //
 
 import Foundation
+
+class Defer_<ElementType> : Sink<ElementType>, ObserverClassType {
+    typealias Parent = Defer<Element>
+    typealias Element = ElementType
+    
+    let parent: Parent
+    
+    init(parent: Parent, observer: ObserverOf<Element>, cancel: Disposable) {
+        self.parent = parent
+        super.init(observer: observer, cancel: cancel)
+    }
+    
+    func run() -> Result<Disposable> {
+        return parent.eval() >== { result in
+            return result.subscribeSafe(ObserverOf(self))
+        } >>! { e in
+            _ = self.observer.on(.Error(e))
+            self.dispose()
+            return success(DefaultDisposable())
+        }
+    }
+    
+    func on(event: Event<Element>) -> Result<Void> {
+        switch event {
+        case .Next:
+            return observer.on(event)
+        case .Error:
+            let result = observer.on(event)
+            dispose()
+            return result
+        case .Completed:
+            let result = observer.on(event)
+            dispose()
+            return result
+        }
+    }
+}
+
+class Defer<Element> : Producer<Element> {
+    typealias Factory = () -> Result<Observable<Element>>
+    
+    let observableFactory : Factory
+    
+    init(observableFactory: Factory) {
+        self.observableFactory = observableFactory
+    }
+    
+    override func run(observer: ObserverOf<Element>, cancel: Disposable, setSink: (Disposable) -> Void) -> Result<Disposable> {
+        let sink = Defer_(parent: self, observer: observer, cancel: cancel)
+        setSink(sink)
+        return sink.run()
+    }
+    
+    func eval() -> Result<Observable<Element>> {
+        return observableFactory()
+    }
+}
