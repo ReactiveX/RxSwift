a29c2edc2592ce3803c1d4544f6b324967a32db1
Krunoslav Zaher <kzaher@Krunoslavs-MacBook-Pro.local>
2015-05-01 22:16:30 +0200

    Fixes cleanup check code.

--- RxExample/RxExample/ViewController.swift
+++ RxExample/RxExample/ViewController.swift
@@ -26,7 +26,8 @@ class ViewController: UIViewController {
         println("View controller disposed with \(resourceCount) resources")
     
         var numberOfResourcesThatShouldRemain = startResourceCount
-        dispatch_async(dispatch_get_main_queue(), { () -> Void in
+        let time = dispatch_time(DISPATCH_TIME_NOW, Int64(0.1 * Double(NSEC_PER_SEC)))
+        dispatch_after(time, dispatch_get_main_queue(), { () -> Void in
             assert(numberOfResourcesThatShouldRemain >= resourceCount, "Resources weren't cleaned properly")
         })
 #endif
