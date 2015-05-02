afc57b7902e5260136d62cbf8a2222a72224a4ed
Krunoslav Zaher <kzaher@Krunoslavs-MacBook-Pro.local>
2015-05-02 15:11:24 +0200

    Removed obsolete function for dispose.

--- RxSwift/RxSwift/Disposable.swift
+++ RxSwift/RxSwift/Disposable.swift
@@ -12,25 +12,3 @@ public protocol Disposable
 {
     func dispose()
 }
-
-public func allSucceedOrDispose(disposables: [Result<Disposable>]) -> Result<Disposable> {
-    let errors = disposables.filter { d in d.error != nil }
-    let numberOfFailures = errors.count
-    if numberOfFailures == 0 {
-        return success(CompositeDisposable(disposables: disposables.map { d in d.value!}))
-    }
-    else {
-        // dispose all of the resources
-        let diposeResult: [Void] = disposables.map { d  in
-            switch d {
-            case .Success(let disposable):
-                disposable.value.dispose()
-                break;
-            case .Error(let error): break
-            }
-            
-            return ()
-        }
-        return createCompositeFailure(errors)
-    }
-}
