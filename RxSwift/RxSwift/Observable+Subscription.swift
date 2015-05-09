43c30ca8b5f3219daecf28448ce550adcb3806b4
Krunoslav Zaher <kzaher@Krunoslavs-MacBook-Pro.local>
2015-05-10 00:05:44 +0200

    Replaces currying with returning lambda values from functions.

--- RxSwift/RxSwift/Observable+Subscription.swift
+++ RxSwift/RxSwift/Observable+Subscription.swift
@@ -10,51 +10,59 @@ import Foundation
 
 public func subscribe<E>
     (on: (event: Event<E>) -> Void)
-    (source: Observable<E>) -> Disposable {
-    let observer = AnonymousObserver { e in
-        on(event: e)
+    -> (Observable<E> -> Disposable) {
+    return { source in
+        let observer = AnonymousObserver { e in
+            on(event: e)
+        }
+        return source.subscribe(observer)
     }
-    return source.subscribe(observer)
 }
 
 public func subscribeNext<E>
     (onNext: (E) -> Void)
-    (source: Observable<E>) -> Disposable {
-    let observer = AnonymousObserver<E> { e in
-        switch e {
-        case .Next(let boxedValue):
-            onNext(boxedValue.value)
-        default:
-            break
+    -> (Observable<E>) -> Disposable {
+    return { source in
+        let observer = AnonymousObserver<E> { e in
+            switch e {
+            case .Next(let boxedValue):
+                onNext(boxedValue.value)
+            default:
+                break
+            }
         }
+        return source.subscribe(observer)
     }
-    return source.subscribe(observer)
 }
 
 public func subscribeError<E>
     (onError: (ErrorType) -> Void)
-    (source: Observable<E>) -> Disposable {
-    let observer = AnonymousObserver<E> { e in
-        switch e {
-        case .Error(let error):
-            onError(error)
-        default:
-            break
+    -> (Observable<E> -> Disposable) {
+    return { source in
+        let observer = AnonymousObserver<E> { e in
+            switch e {
+            case .Error(let error):
+                onError(error)
+            default:
+                break
+            }
         }
+        return source.subscribe(observer)
     }
-    return source.subscribe(observer)
 }
 
 public func subscribeCompleted<E>
     (onCompleted: () -> Void)
-    (source: Observable<E>) -> Disposable {
-    let observer = AnonymousObserver<E> { e in
-        switch e {
-        case .Completed:
-            onCompleted()
-        default:
-            break
+    -> (Observable<E> -> Disposable) {
+    return { source in
+        let observer = AnonymousObserver<E> { e in
+            switch e {
+            case .Completed:
+                onCompleted()
+            default:
+                break
+            }
         }
+        return source.subscribe(observer)
     }
-    return source.subscribe(observer)
 }
