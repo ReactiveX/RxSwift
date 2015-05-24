c69d2f76685650012508279d0b5bb4039eb4b610
Daniel Tartaglia <dtartaglia@hanekedesign.com>
2015-05-24 15:36:31 -0400

    Added an OS XPlayground for documentation.

---
+++ OSXPlayground.playground/Sources/SupportCode.swift
@@ -0,0 +1,3 @@
+//
+// This file (and all other Swift source files in the Sources directory of this playground) will be precompiled into a framework which is automatically made available to OSXPlayground.playground.
+//
