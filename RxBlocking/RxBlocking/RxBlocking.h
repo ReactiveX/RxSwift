ef36901cff41dcc9c6a2a9b8e9f732f8144d4681
Krunoslav Zaher <krunoslav.zaher@gmail.com>
2015-07-12 18:44:56 +0200

    Adds blocking operators.

---
+++ RxBlocking/RxBlocking/RxBlocking.h
@@ -0,0 +1,19 @@
+//
+//  RxBlocking.h
+//  RxBlocking
+//
+//  Created by Krunoslav Zaher on 7/12/15.
+//  Copyright (c) 2015 Krunoslav Zaher. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RxBlocking.
+FOUNDATION_EXPORT double RxBlockingVersionNumber;
+
+//! Project version string for RxBlocking.
+FOUNDATION_EXPORT const unsigned char RxBlockingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxBlocking/PublicHeader.h>
+
+
---
+++ RxTests/RxSwiftTests/Tests/Observable+BlockingTest.swift
@@ -0,0 +1,108 @@
+//
+//  Observable+BlockingTest.swift
+//  RxTests
+//
+//  Created by Krunoslav Zaher on 7/12/15.
+//
+//
+
+import Foundation
+import RxSwift
+import RxBlocking
+import XCTest
+
+class ObservableBlockingTest : RxTest {
+    
+}
+
+// toArray
+
+extension ObservableBlockingTest {
+    func testToArray_empty() {
+        XCTAssert((empty() as Observable<Int> >- toArray).get() == [])
+    }
+    
+    func testToArray_return() {
+        XCTAssert((just(42) >- toArray).get() == [42])
+    }
+    
+    func testToArray_fail() {
+        XCTAssert((failWith(testError) as Observable<Int> >- toArray).isFailure)
+    }
+    
+    func testToArray_someData() {
+        XCTAssert((returnElements(42, 43, 44, 45) >- toArray).get() == [42, 43, 44, 45])
+    }
+    
+    func testToArray_withRealScheduler() {
+        let scheduler = ConcurrentDispatchQueueScheduler(globalConcurrentQueuePriority: .Default)
+        
+        let array = interval(0.001, scheduler)
+            >- take(10)
+            >- toArray
+        
+        XCTAssert(array.get() == Array(0..<10))
+    }
+}
+
+// first
+
+extension ObservableBlockingTest {
+    func testFirst_empty() {
+        XCTAssert((empty() as Observable<Int> >- first).get() == nil)
+    }
+    
+    func testFirst_return() {
+        XCTAssert((just(42) >- first).get() == 42)
+    }
+    
+    func testFirst_fail() {
+        XCTAssert((failWith(testError) as Observable<Int> >- first).isFailure)
+    }
+    
+    func testFirst_someData() {
+        XCTAssert((returnElements(42, 43, 44, 45) >- first).get() == 42)
+    }
+    
+    func testFirst_withRealScheduler() {
+        let scheduler = ConcurrentDispatchQueueScheduler(globalConcurrentQueuePriority: .Default)
+        
+        let array = interval(0.001, scheduler)
+            >- take(10)
+            >- first
+        
+        XCTAssert(array.get() == 0)
+    }
+}
+
+// last
+
+extension ObservableBlockingTest {
+    func testLast_empty() {
+        XCTAssert((empty() as Observable<Int> >- last).get() == nil)
+    }
+    
+    func testLast_return() {
+        XCTAssert((just(42) >- last).get() == 42)
+    }
+    
+    func testLast_fail() {
+        XCTAssert((failWith(testError) as Observable<Int> >- last).isFailure)
+    }
+    
+    func testLast_someData() {
+        XCTAssert((returnElements(42, 43, 44, 45) >- last).get() == 45)
+    }
+    
+    func testLast_withRealScheduler() {
+        let scheduler = ConcurrentDispatchQueueScheduler(globalConcurrentQueuePriority: .Default)
+        
+        let array = interval(0.001, scheduler)
+            >- take(10)
+            >- last
+        
+        XCTAssert(array.get() == 9)
+    }
+}
+
+
