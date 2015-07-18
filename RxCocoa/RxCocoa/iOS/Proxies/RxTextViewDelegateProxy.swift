92cb0ce4e29dc3f1ec2093ca43f1c5675e7d31db
tokorom <tokorom@gmail.com>
2015-07-19 05:51:24 +0900

    Add UITextView+Rx

---
+++ RxCocoa/RxCocoa/iOS/Proxies/RxTextViewDelegateProxy.swift
@@ -0,0 +1,17 @@
+//
+//  RxTextViewDelegateProxy.swift
+//  RxCocoa
+//
+//  Created by Yuta ToKoRo on 7/19/15.
+//  Copyright (c) 2015 Krunoslav Zaher. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import RxSwift
+
+// Please take a look at `DelegateProxyType.swift`
+class RxTextViewDelegateProxy : RxScrollViewDelegateProxy
+                               , UITextViewDelegate {
+    
+}
