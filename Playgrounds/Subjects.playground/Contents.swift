554addd805c4ea09d70504cc4aab149bccdea8d7
Leonard <lfaoro@me.com>
2015-08-15 19:00:59 +0200

    Polished playgrounds
    All errors have been fixed for PG:
    * Introduction
    * Subjects

--- Playgrounds/Subjects.playground/Contents.swift
+++ Playgrounds/Subjects.playground/Contents.swift
@@ -15,55 +15,55 @@ func writeSequenceToConsole(name: String, sequence: Observable<String>) {
 }
 
 
-ex/*:
+/*:
 
 ## PublishSubject
 
 PublishSubject can begin emitting items immediately upon creation, but there is a risk that one or more items may be lost between the time the Subject is created and the observer subscribes to it.
 
 */
-ample("PublishSubject") {
+example("PublishSubject") {
     let subject = PublishSubject<String>()
-    writeSequenceToConsole("1", subject)
+    writeSequenceToConsole("1", sequence: subject)
     sendNext(subject, "a")
     sendNext(subject, "b")
-    writeSequenceToConsole("2", subject)
+    writeSequenceToConsole("2", sequence: subject)
     sendNext(subject, "c")
     sendNext(subject, "d")
 }
 
 
-ex/*:
+/*:
 
 ## ReplaySubject
 
 ReplaySubject emits to any observer all of the items, in the buffer, that were emitted by the source 
 
 */
-ample("ReplaySubject") {
+example("ReplaySubject") {
     let subject = ReplaySubject<String>(bufferSize: 1)
-    writeSequenceToConsole("1", subject)
+    writeSequenceToConsole("1", sequence: subject)
     sendNext(subject, "a")
     sendNext(subject, "b")
-    writeSequenceToConsole("2", subject)
+    writeSequenceToConsole("2", sequence: subject)
     sendNext(subject, "c")
     sendNext(subject, "d")
 }
 
 
-ex/*:
+/*:
 
 ## BehaviorSubject a.k.a. Variable
 
 ReplaySubject emits to any observer all of the items, in the buffer, that were emitted by the source 
 
 */
-ample("ReplaySubject") {
+example("ReplaySubject") {
     let subject = BehaviorSubject(value: "z")
-    writeSequenceToConsole("1", subject)
+    writeSequenceToConsole("1", sequence: subject)
     sendNext(subject, "a")
     sendNext(subject, "b")
-    writeSequenceToConsole("2", subject)
+    writeSequenceToConsole("2", sequence: subject)
     sendNext(subject, "c")
     sendNext(subject, "d")
 }
