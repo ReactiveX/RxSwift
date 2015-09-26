b950f9da764747556500b78aca68de756b4bfa03
Tomi Koskinen <tomi.koskinen@welho.com>
2015-09-26 12:56:43 +0300

    Adds `average` operator

--- RxSwift/Observables/Observable+Aggregate.swift
+++ RxSwift/Observables/Observable+Aggregate.swift
@@ -41,3 +41,63 @@ extension ObservableType {
         return Reduce(source: self.asObservable(), seed: seed, accumulator: accumulator, mapResult: { $0 })
     }
 }
+
+
+// average
+
+extension ObservableType where E == Int {
+
+    /**
+    Calculates the `average` over an observable Int sequence, returning the result as a single Double value.
+    
+    - returns: An observable sequence containing a single Double element with the average value.
+    */
+    public func average() -> Observable<Double> {
+        let source = self.asObservable()
+        typealias Acc = (Double, Int)
+        return source.reduce(Acc(0, 0), { (acc: Acc, element: Int) -> Acc in
+            Acc(acc.0 + Double(element), acc.1 + 1)
+            })
+            { (result: Acc) -> Double in
+                result.0 / Double(result.1)
+        }
+    }
+}
+
+extension ObservableType where E == Float {
+
+    /**
+    Calculates the `average` over an observable Float sequence, returning the result as a single Float value.
+    
+    - returns: An observable sequence containing a single Float element with the average value.
+    */
+    public func average() -> Observable<Float> {
+        let source = self.asObservable()
+        typealias Acc = (Float, Int)
+        return source.reduce(Acc(0, 0), { (acc: Acc, element: Float) -> Acc in
+            Acc(acc.0 + element, acc.1 + 1)
+            })
+            { (result: Acc) -> Float in
+                return result.0 / Float(result.1)
+        }
+    }
+}
+
+extension ObservableType where E == Double {
+
+    /**
+    Calculates the `average` over an observable Double sequence, returning the result as a single Double value.
+    
+    - returns: An observable sequence containing a single Double element with the average value.
+    */
+    public func average() -> Observable<Double> {
+        let source = self.asObservable()
+        typealias Acc = (Double, Double)
+        return source.reduce(Acc(0, 0), { (acc: Acc, element: Double) -> Acc in
+            Acc(acc.0 + element, acc.1 + 1)
+            })
+            { (result: Acc) -> Double in
+                result.0 / result.1
+        }
+    }
+}
--- RxTests/RxSwiftTests/Tests/Observable+AggregateTest.swift
+++ RxTests/RxSwiftTests/Tests/Observable+AggregateTest.swift
@@ -346,3 +346,303 @@ extension ObservableAggregateTest {
         XCTAssertEqual(xs.subscriptions, correctSubscriptions)
     }
 }
+
+
+
+
+
+class ObservableAverageTest : RxTest {
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+}
+
+
+extension ObservableAverageTest {
+    func test_AverageInteger_Never() {
+        let scheduler = TestScheduler(initialClock: 0)
+        
+        let xs = scheduler.createHotObservable([
+            next(150, 1),
+            ])
+        
+        let res = scheduler.start { xs.average() }
+        
+        let correctMessages: [Recorded<Double>] = [
+        ]
+        
+        let correctSubscriptions = [
+            Subscription(200, 1000)
+        ]
+        
+        XCTAssertEqual(res.messages, correctMessages)
+        XCTAssertEqual(xs.subscriptions, correctSubscriptions)
+    }
+    
+    func test_AverageInteger_Empty() {
+        let scheduler = TestScheduler(initialClock: 0)
+        
+        let xs = scheduler.createHotObservable([
+            next(150, 1),
+            completed(250)
+            ])
+        
+        let res = scheduler.start {
+            xs.average()
+        }
+        
+        XCTAssertEqual(res.messages[0].time, 250)
+        XCTAssertTrue(res.messages[0].value.isNaN)
+        XCTAssertEqual(res.messages[1], completed(250))
+        
+        XCTAssertEqual(xs.subscriptions, [
+            Subscription(200, 250)
+            ])
+    }
+
+    func test_AverageInteger_Single() {
+        let scheduler = TestScheduler(initialClock: 0)
+        
+        let xs = scheduler.createHotObservable([
+            next(150, 1),
+            next(210, 2),
+            completed(220)
+            ])
+        
+        let res = scheduler.start {
+            xs.average()
+        }
+        
+        let messages: [Recorded<Double>] = [
+            next(220, 2.0),
+            completed(220)
+        ]
+        
+        XCTAssertEqual(res.messages, messages)
+        
+        XCTAssertEqual(xs.subscriptions, [
+            Subscription(200, 220)
+            ])
+    }
+    
+    func test_AverageInteger_Range() {
+        let scheduler = TestScheduler(initialClock: 0)
+        
+        let xs = scheduler.createHotObservable([
+            next(150, 1),
+            next(210, 2),
+            next(220, 3),
+            next(230, 4),
+            next(240, 5),
+            completed(250)
+            ])
+        
+        let res = scheduler.start {
+            xs.average()
+        }
+        
+        let messages: [Recorded<Double>] = [
+            next(250, 3.5),
+            completed(250)
+        ]
+        
+        XCTAssertEqual(res.messages, messages)
+        
+        XCTAssertEqual(xs.subscriptions, [
+            Subscription(200, 250)
+            ])
+    }
+
+    func test_AverageFloat_Never() {
+        let scheduler = TestScheduler(initialClock: 0)
+        
+        let xs = scheduler.createHotObservable([
+            next(150, Float(1)),
+            ])
+        
+        let res = scheduler.start { xs.average() }
+        
+        let correctMessages: [Recorded<Float>] = [
+        ]
+        
+        let correctSubscriptions = [
+            Subscription(200, 1000)
+        ]
+        
+        XCTAssertEqual(res.messages, correctMessages)
+        XCTAssertEqual(xs.subscriptions, correctSubscriptions)
+    }
+    
+    func test_AverageFloat_Empty() {
+        let scheduler = TestScheduler(initialClock: 0)
+        
+        let xs = scheduler.createHotObservable([
+            next(150, Float(1.111)),
+            completed(250)
+            ])
+        
+        let res = scheduler.start {
+            xs.average()
+        }
+        
+        XCTAssertEqual(res.messages[0].time, 250)
+        XCTAssertTrue(res.messages[0].value.isNaN)
+        XCTAssertEqual(res.messages[1], completed(250))
+        
+        XCTAssertEqual(xs.subscriptions, [
+            Subscription(200, 250)
+            ])
+    }
+
+    func test_AverageFloat_Single() {
+        let scheduler = TestScheduler(initialClock: 0)
+        
+        let xs = scheduler.createHotObservable([
+            next(150, Float(1.111)),
+            next(210, Float(2.0)),
+            completed(220)
+            ])
+        
+        let res = scheduler.start {
+            xs.average()
+        }
+        
+        let messages: [Recorded<Float>] = [
+            next(220, 2.0),
+            completed(220)
+        ]
+        
+        XCTAssertEqual(res.messages, messages)
+        
+        XCTAssertEqual(xs.subscriptions, [
+            Subscription(200, 220)
+            ])
+    }
+
+    func test_AverageFloat_Range() {
+        let scheduler = TestScheduler(initialClock: 0)
+        
+        let xs = scheduler.createHotObservable([
+            next(150, Float(1.111)),
+            next(210, Float(2.22)),
+            next(220, Float(3.33)),
+            next(230, Float(4.33)),
+            next(240, Float(5.111)),
+            completed(250)
+            ])
+        
+        let res = scheduler.start {
+            xs.average()
+        }
+        
+        let messages: [Recorded<Float>] = [
+            next(250, 3.74775),
+            completed(250)
+        ]
+        
+        XCTAssertEqual(res.messages, messages)
+        
+        XCTAssertEqual(xs.subscriptions, [
+            Subscription(200, 250)
+            ])
+    }
+
+    func test_AverageDouble_Never() {
+        let scheduler = TestScheduler(initialClock: 0)
+        
+        let xs = scheduler.createHotObservable([
+            next(150, Double(1)),
+            ])
+        
+        let res = scheduler.start { xs.average() }
+        
+        let correctMessages: [Recorded<Double>] = [
+        ]
+        
+        let correctSubscriptions = [
+            Subscription(200, 1000)
+        ]
+        
+        XCTAssertEqual(res.messages, correctMessages)
+        XCTAssertEqual(xs.subscriptions, correctSubscriptions)
+    }
+    
+    func test_AverageDouble_Empty() {
+        let scheduler = TestScheduler(initialClock: 0)
+        
+        let xs = scheduler.createHotObservable([
+            next(150, Double(1.111)),
+            completed(250)
+            ])
+        
+        let res = scheduler.start {
+            xs.average()
+        }
+        
+        XCTAssertEqual(res.messages[0].time, 250)
+        XCTAssertTrue(res.messages[0].value.isNaN)
+        XCTAssertEqual(res.messages[1], completed(250))
+        
+        XCTAssertEqual(xs.subscriptions, [
+            Subscription(200, 250)
+            ])
+    }
+
+    func test_AverageDouble_Single() {
+        let scheduler = TestScheduler(initialClock: 0)
+        
+        let xs = scheduler.createHotObservable([
+            next(150, Double(1.111)),
+            next(210, Double(2.0)),
+            completed(220)
+            ])
+        
+        let res = scheduler.start {
+            xs.average()
+        }
+        
+        let messages: [Recorded<Double>] = [
+            next(220, 2.0),
+            completed(220)
+        ]
+        
+        XCTAssertEqual(res.messages, messages)
+        
+        XCTAssertEqual(xs.subscriptions, [
+            Subscription(200, 220)
+            ])
+    }
+    
+    func test_AverageDouble_Range() {
+        let scheduler = TestScheduler(initialClock: 0)
+        
+        let xs = scheduler.createHotObservable([
+            next(150, Double(1.111)),
+            next(210, Double(2.22)),
+            next(220, Double(3.33)),
+            next(230, Double(4.33)),
+            next(240, Double(5.111)),
+            completed(250)
+            ])
+        
+        let res = scheduler.start {
+            xs.average()
+        }
+        
+        let messages: [Recorded<Double>] = [
+            next(250, 3.74775),
+            completed(250)
+        ]
+        
+        XCTAssertEqual(res.messages, messages)
+        
+        XCTAssertEqual(xs.subscriptions, [
+            Subscription(200, 250)
+            ])
+    }
+
+}
