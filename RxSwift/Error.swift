c66723de70cdca613ad0aeb0d56cd21a6d51ba0a
Krunoslav Zaher <krunoslav.zaher@gmail.com>
2015-10-18 15:05:44 +0200

    Updates Error documentation.

--- RxSwift/Error.swift
+++ RxSwift/Error.swift
@@ -13,15 +13,23 @@ let RxCompositeFailures = "RxCompositeFailures"
 
 /**
 Generic Rx error codes.
-
-- Unknown: Unknown error occured
-- Cast: Error during Casting
-- Disposed: Performing an action on disposed object
 */
 public enum RxErrorCode : Int {
+    /**
+    Unknown error occured
+    */
     case Unknown   = 0
+    /**
+    Casting error
+    */
     case Cast      = 2
+    /**
+    Performing an action on disposed object
+    */
     case Disposed  = 3
+    /**
+    Aritmetic overflow error.
+    */
     case Overflow  = 4
 }
 
@@ -30,20 +38,23 @@ Singleton instances of RxErrors
 */
 public struct RxError {
     /**
-    Singleton instance of Unknown Error
+    Singleton instance of unknown Error
     */
     public static let UnknownError  = NSError(domain: RxErrorDomain, code: RxErrorCode.Unknown.rawValue, userInfo: nil)
 
     /**
-    Singleton instance of error during casting
+    Singleton instance of error during casting.
     */
     public static let CastError     = NSError(domain: RxErrorDomain, code: RxErrorCode.Cast.rawValue, userInfo: nil)
 
     /**
-    Singleton instance of doing something on a disposed object
+    Singleton instance of doing something on a disposed object error.
     */
     public static let DisposedError = NSError(domain: RxErrorDomain, code: RxErrorCode.Disposed.rawValue, userInfo: nil)
-    
+
+    /**
+    Singleton instance of aritmetic overflow error.
+    */
     public static let OverflowError = NSError(domain: RxErrorDomain, code: RxErrorCode.Overflow.rawValue, userInfo: nil)
 
 }
