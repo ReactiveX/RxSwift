a35c77c0e04c03ef4807e3dc51e04e39c32e766f
Vodovozov Gleb <vodovozovge@gmail.com>
2015-10-24 11:20:18 +0800

    Reachability+Rx now is ReachabilityService

---
+++ RxExample/RxExample/Services/ReachabilityService.swift
@@ -0,0 +1,54 @@
+//
+//  ReachabilityService.swift
+//  RxExample
+//
+//  Created by Vodovozov Gleb on 22.10.2015.
+//  Copyright © 2015 Krunoslav Zaher. All rights reserved.
+//
+
+import RxSwift
+
+public enum ReachabilityStatus{
+    case Reachable,Unreachable
+}
+
+class ReachabilityService{
+
+    private let reachabilityRef = try! Reachability.reachabilityForInternetConnection()
+
+    private let _reachabilityChangedSubject = PublishSubject<ReachabilityStatus>()
+    private var reachabilityChanged:Observable<ReachabilityStatus> {
+        get{
+            return _reachabilityChangedSubject.asObservable()
+        }
+    }
+
+    // singleton
+    static let sharedReachabilityService = ReachabilityService()
+
+    init(){
+        reachabilityRef.whenReachable = { reachability in
+            self._reachabilityChangedSubject.on(.Next(.Reachable))
+        }
+
+        reachabilityRef.whenUnreachable = { reachability in
+            self._reachabilityChangedSubject.on(.Next(.Unreachable))
+        }
+
+        try! reachabilityRef.startNotifier()
+
+    }
+}
+
+extension ObservableConvertibleType {
+    func retryOnBecomesReachable(valueOnFailure:E, reachabilityService:ReachabilityService)->Observable<E>{
+        return self.asObservable()
+            .catchError { (e) -> Observable<E> in
+                reachabilityService
+                    .reachabilityChanged
+                    .flatMap { _ in failWith(e) }
+                    .startWith(valueOnFailure)
+            }
+            .retry()
+    }
+}
