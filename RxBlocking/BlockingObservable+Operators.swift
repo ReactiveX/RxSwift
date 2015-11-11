5499c10f9a61a2ab815f33066727b7215fcecc95
Tomi Koskinen <tomi.koskinen@reaktor.fi>
2015-11-11 18:43:22 +0200

    remove duplicate blocking single code

--- RxBlocking/BlockingObservable+Operators.swift
+++ RxBlocking/BlockingObservable+Operators.swift
@@ -155,47 +155,7 @@ extension BlockingObservable {
      - returns: Returns the only element of an sequence, and reports an exception if there is not exactly one element in the observable sequence.
      */
     public func single() throws -> E? {
-        var element: E?
-        
-        var error: ErrorType?
-        
-        let d = SingleAssignmentDisposable()
-        
-        let lock = RunLoopLock()
-        
-        lock.dispatch {
-            d.disposable = self.source.subscribe { e in
-                switch e {
-                case .Next(let e):
-                    if element == nil {
-                        element = e
-                    } else {
-                        error = RxError.MoreThanOneElement
-                    }
-                    return
-                case .Error(let e):
-                    error = e
-                    break
-                case .Completed:
-                    if element == nil {
-                        error = RxError.NoElements
-                    }
-                    break
-                }
-                
-                lock.stop()
-            }
-        }
-        
-        lock.run()
-        
-        d.dispose()
-        
-        if let error = error {
-            throw error
-        }
-
-        return element
+        return try single { _ in true }
     }
 
     /**
