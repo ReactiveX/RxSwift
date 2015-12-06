ca1488841cdec6603fe684a45d588c5b33f39856
Krunoslav Zaher <krunoslav.zaher@gmail.com>
2015-12-06 20:58:16 +0100

    Adds simple validation example.

--- RxExample/RxExample/Examples/SimpleValidation/SimpleValidationViewController.swift
+++ RxExample/RxExample/Examples/SimpleValidation/SimpleValidationViewController.swift
@@ -13,49 +13,66 @@ import RxSwift
 import RxCocoa
 #endif
 
-let mininalUsernameLength = 5
-let mininalPasswordLength = 5
+let minimalUsernameLength = 5
+let minimalPasswordLength = 5
 
 class SimpleValidationViewController : ViewController {
 
-    @IBOutlet weak var username: UITextField!
-    @IBOutlet weak var usernameValid: UILabel!
+    @IBOutlet weak var usernameOutlet: UITextField!
+    @IBOutlet weak var usernameValidOutlet: UILabel!
 
-    @IBOutlet weak var password: UITextField!
-    @IBOutlet weak var passwordValid: UILabel!
+    @IBOutlet weak var passwordOutlet: UITextField!
+    @IBOutlet weak var passwordValidOutlet: UILabel!
 
-    @IBOutlet weak var doSomething: UIButton!
-
-    func showAlert() {
-        DefaultWireframe.sharedInstance.promptFor("Something wonderful has been done", cancelAction: "OK", actions: [])
-    }
+    @IBOutlet weak var doSomethingOutlet: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        let usernameValid = username.rx_text
-            .map { $0.characters.count >= mininalUsernameLength }
+        usernameValidOutlet.text = "Username has to be at least \(minimalUsernameLength) characters"
+        passwordValidOutlet.text = "Username has to be at least \(minimalPasswordLength) characters"
+
+        let usernameValid = usernameOutlet.rx_text
+            .map { $0.characters.count >= minimalUsernameLength }
             .shareReplay(1) // without this map would be executed once for each binding, rx is stateless by default
 
-        let passwordValid = password.rx_text
-            .map { $0.characters.count >= mininalPasswordLength }
+        let passwordValid = passwordOutlet.rx_text
+            .map { $0.characters.count >= minimalPasswordLength }
             .shareReplay(1)
 
         let everythingValid = combineLatest(usernameValid, passwordValid) { $0 && $1 }
             .shareReplay(1)
 
-        
+        usernameValid
+            .bindTo(passwordOutlet.rx_enabled)
+            .addDisposableTo(disposeBag)
 
         usernameValid
-            .bindTo(password.rx_enabled)
+            .bindTo(usernameValidOutlet.rx_hidden)
+            .addDisposableTo(disposeBag)
+
+        passwordValid
+            .bindTo(passwordValidOutlet.rx_hidden)
             .addDisposableTo(disposeBag)
 
         everythingValid
-            .bindTo(doSomething.rx_enabled)
+            .bindTo(doSomethingOutlet.rx_enabled)
             .addDisposableTo(disposeBag)
 
-        doSomething.rx_tap
+        doSomethingOutlet.rx_tap
             .subscribeNext(showAlert)
             .addDisposableTo(disposeBag)
     }
+
+    func showAlert() {
+        let alertView = UIAlertView(
+            title: "RxExample",
+            message: "This is wonderful",
+            delegate: nil,
+            cancelButtonTitle: "OK"
+        )
+
+        alertView.show()
+    }
+
 }
