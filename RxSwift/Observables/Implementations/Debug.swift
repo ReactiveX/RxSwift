c90511b99b4be627c303ba1e7ef9fc6ef2a4e5c9
yury <yury.korolev@gmail.com>
2015-12-16 13:48:13 +0300

    Move NSDateFormatter to instance var. Add timestamp to dispose

--- RxSwift/Observables/Implementations/Debug.swift
+++ RxSwift/Observables/Implementations/Debug.swift
@@ -13,9 +13,11 @@ class Debug_<O: ObserverType> : Sink<O>, ObserverType {
     typealias Parent = Debug<Element>
     
     private let _parent: Parent
+    private let _timestampFormatter = NSDateFormatter()
     
     init(parent: Parent, observer: O) {
         _parent = parent
+        _timestampFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss.SSS"
         super.init(observer: observer)
     }
     
@@ -26,14 +28,12 @@ class Debug_<O: ObserverType> : Sink<O>, ObserverType {
             ? String(eventText.characters.prefix(maxEventTextLength / 2)) + "..." + String(eventText.characters.suffix(maxEventTextLength / 2))
             : eventText
 
-        let format = NSDateFormatter()
-        format.dateFormat = "yyyy-MM-dd HH:mm:ss.SSS"
-        print("\(format.stringFromDate(NSDate())): [\(_parent._identifier)] -> Event \(eventNormalized)")
+        print("\(_timestampFormatter.stringFromDate(NSDate())): [\(_parent._identifier)] -> Event \(eventNormalized)")
         forwardOn(event)
     }
     
     override func dispose() {
-        print("[\(_parent._identifier)] dispose")
+        print("\(_timestampFormatter.stringFromDate(NSDate())): [\(_parent._identifier)] dispose")
         super.dispose()
     }
 }
