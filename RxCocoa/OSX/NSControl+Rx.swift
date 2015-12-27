a28bb181c027e49dceb61217105005e41ca09875
mrahmiao <mrahmiao@gmail.com>
2015-12-27 12:43:11 +0800

    Adds bindable 'rx_enabled' to NSControl
    Signed-off-by: mrahmiao <mrahmiao@gmail.com>

--- RxCocoa/OSX/NSControl+Rx.swift
+++ RxCocoa/OSX/NSControl+Rx.swift
@@ -42,6 +42,25 @@ extension NSControl {
         
         return ControlEvent(events: source)
     }
+  
+    /**
+     Bindable sink for `enabled` property.
+     */
+    public var rx_enabled: AnyObserver<Bool> {
+        return AnyObserver { [weak self] event in
+            MainScheduler.ensureExecutingOnScheduler()
+            
+            switch event {
+            case .Next(let value):
+                self?.enabled = value
+            case .Error(let error):
+                bindingErrorToInterface(error)
+                break
+            case .Completed:
+                break
+            }
+        }
+    }
 
     /**
     Helper to make sure that `Observable` returned from `createCachedObservable` is only created once.
