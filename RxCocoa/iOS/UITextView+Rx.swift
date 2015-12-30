32d048fe05553d2508ef1f5dc7d0de59244e0c8d
sshulga <sergii.shulga@infopulse.com>
2015-12-30 14:58:47 +0200

    Fixes for UITextView rx_text bug with autocorrect enabled #333

--- RxCocoa/iOS/UITextView+Rx.swift
+++ RxCocoa/iOS/UITextView+Rx.swift
@@ -14,6 +14,8 @@ import UIKit
 import RxSwift
 #endif
 
+    
+    
 extension UITextView {
     
     /**
@@ -29,31 +31,7 @@ extension UITextView {
     Reactive wrapper for `text` property.
     */
     public var rx_text: ControlProperty<String> {
-        let source: Observable<String> = Observable.deferred { [weak self] () -> Observable<String> in
-            let text = self?.text ?? ""
-            // basic event
-            let textChangedEvent = self?.rx_delegate.observe("textViewDidChange:") ?? Observable.empty()
-
-            // Monitor all other events because text could change without user intervention and without
-            // `textViewDidChange:` firing.
-            // For example, autocorrecting spell checker.
-            let anyOtherEvent = (self?.rx_delegate as? RxTextViewDelegateProxy)?.textChanging ?? Observable.empty()
-
-            // Throttle is here because textChanging will fire when text is about to change.
-            // Event needs to happen after text is changed. This is kind of a hacky way, but
-            // don't know any other way for now.
-            let throttledAnyOtherEvent = anyOtherEvent
-                .throttle(0, scheduler: MainScheduler.sharedInstance)
-                .takeUntil(self?.rx_deallocated ?? Observable.just())
-
-            return Observable.of(textChangedEvent.map { _ in () }, throttledAnyOtherEvent)
-                .merge()
-                .map { a in
-                    return self?.text ?? ""
-                }
-                .startWith(text)
-                .distinctUntilChanged()
-            }
+        let source: Observable<String> = textStorage.rx_string
         
         return ControlProperty(values: source, valueSink: AnyObserver { [weak self] event in
             switch event {
