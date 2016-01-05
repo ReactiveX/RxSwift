fe4dda0ed7e7b3a098841463b51cc37072ded4a7
sshulga <sergii.shulga@infopulse.com>
2016-01-05 15:15:15 +0200

    Rx wrappers for UIImagePickerController

---
+++ RxCocoa/iOS/Proxies/RxImagePickerDelegateProxy.swift
@@ -0,0 +1,41 @@
+//
+//  RxImagePickerDelegateProxy.swift
+//  Rx
+//
+//  Created by Segii Shulga on 1/4/16.
+//  Copyright © 2016 Krunoslav Zaher. All rights reserved.
+//
+
+#if os(iOS)
+   
+   import Foundation
+#if !RX_NO_MODULE
+   import RxSwift
+#endif
+   import UIKit
+
+public class RxImagePickerDelegateProxy
+    : DelegateProxy
+    , DelegateProxyType
+    , UIImagePickerControllerDelegate
+    , UINavigationControllerDelegate {
+    
+    /**
+     For more information take a look at `DelegateProxyType`.
+     */
+    public class func setCurrentDelegate(delegate: AnyObject?, toObject object: AnyObject) {
+        let imagePickerController: UIImagePickerController = castOrFatalError(object)
+        imagePickerController.delegate = castOptionalOrFatalError(delegate)
+    }
+    
+    /**
+     For more information take a look at `DelegateProxyType`.
+     */
+    public class func currentDelegateFor(object: AnyObject) -> AnyObject? {
+        let imagePickerController: UIImagePickerController = castOrFatalError(object)
+        return imagePickerController.delegate
+    }
+
+}
+   
+#endif
