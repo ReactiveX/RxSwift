a25cdd598c409d3734f9f3a3bd5b9b276ab0509e
sergdort <sergdort@gmail.com>
2016-01-06 19:28:31 +0200

    Added tests for UIImagePickerController+Rx

---
+++ Tests/RxCocoaTests/UIImagePickerController+RxTests.swift
@@ -0,0 +1,74 @@
+//
+//  UIImagePickerController+RxTests.swift
+//  Rx
+//
+//  Created by Segii Shulga on 1/6/16.
+//  Copyright © 2016 Krunoslav Zaher. All rights reserved.
+//
+
+
+
+#if os(iOS)
+    
+import Foundation
+import RxSwift
+import RxCocoa
+import XCTest
+import UIKit
+    
+class UIImagePickerControllerTests: RxTest {
+    
+}
+
+extension UIImagePickerControllerTests {
+    
+    func testDidFinishPickingMediaWithInfo() {
+        var completed = false
+        var info:[String:AnyObject]?
+        
+        let pickedInfo = [UIImagePickerControllerOriginalImage : UIImage()]
+        
+        autoreleasepool {
+            let imagePickerController = UIImagePickerController()
+            
+            _ = imagePickerController.rx_didFinishPickingMediaWithInfo
+                .subscribe(onNext: { (i) -> Void in
+                    info = i
+                }, onCompleted: {
+                    completed = true
+                })
+            
+            imagePickerController.delegate!
+                .imagePickerController!(imagePickerController,didFinishPickingMediaWithInfo:pickedInfo)
+            
+            
+        }
+        
+        XCTAssertTrue(info?[UIImagePickerControllerOriginalImage] === pickedInfo[UIImagePickerControllerOriginalImage])
+        XCTAssertTrue(completed)
+    }
+    
+    func testDidCancel() {
+        var completed = false
+        var canceled = false
+        
+        autoreleasepool {
+            
+            let imagePickerController = UIImagePickerController()
+            
+            _ = imagePickerController.rx_didCancel
+                .subscribe(onNext: { (i) -> Void in
+                        canceled = true
+                    }, onCompleted: {
+                        completed = true
+                })
+            imagePickerController.delegate!.imagePickerControllerDidCancel!(imagePickerController)
+            
+        }
+        XCTAssertTrue(canceled)
+        XCTAssertTrue(completed)
+    }
+    
+}
+    
+#endif
