ffc976dac9a1528cf1381ab0c894e0b99e1fe8c3
Krunoslav Zaher <krunoslav.zaher@gmail.com>
2016-01-10 18:33:01 +0100

    Refactors `UIImagePickerController` to use more idiomatic coding style.

--- RxExample/RxExample/Examples/ImagePicker/ImagePickerController.swift
+++ RxExample/RxExample/Examples/ImagePicker/ImagePickerController.swift
@@ -9,89 +9,69 @@
 import Foundation
 import UIKit
 #if !RX_NO_MODULE
-   import RxSwift
-   import RxCocoa
+    import RxSwift
+    import RxCocoa
 #endif
-
-struct ImagePickerConfiguration {
-   let soruceType:UIImagePickerControllerSourceType
-   let allowsEditing:Bool
-}
 
 class ImagePickerController: ViewController {
 
-   @IBOutlet var imageView: UIImageView!
-   @IBOutlet var cameraButton: UIButton!
-   @IBOutlet var galleryButton: UIButton!
-   @IBOutlet var cropButton: UIButton!
-   
-   let imagePickerController = UIImagePickerController()
-   
-   private(set) lazy var showImagePickerObserver:AnyObserver<ImagePickerConfiguration> = {
-      return AnyObserver { [weak self] event in
-         switch event {
-         case .Next(let configuration):
-            guard let strong = self else {return}
-            strong.imagePickerController.sourceType = configuration.soruceType
-            strong.imagePickerController.allowsEditing = configuration.allowsEditing
-            strong.presentViewController(strong.imagePickerController,
-               animated: true,
-               completion: nil)
-         default:
-            break;
-         }
-      }
-   }()
-   
-   override func viewDidLoad() {
-      super.viewDidLoad()
-      
-      imagePickerController
-         .rx_didFinishPickingMediaWithInfo
-         .filter({[weak self] _ in return (self?.imagePickerController.allowsEditing ?? false) == false})
-         .map { (info) in
-            return info[UIImagePickerControllerOriginalImage] as? UIImage
-         }
-         .bindTo(imageView.rx_image)
-         .addDisposableTo(disposeBag)
-      
-      imagePickerController
-         .rx_didFinishPickingMediaWithInfo
-         .filter({[weak self] _ in return (self?.imagePickerController.allowsEditing ?? false)})
-         .map { (info) in
-            return info[UIImagePickerControllerEditedImage] as? UIImage
-         }
-         .bindTo(imageView.rx_image)
-         .addDisposableTo(disposeBag)
-      
-      
-      cameraButton.enabled = UIImagePickerController.isSourceTypeAvailable(.Camera)
-      
-      cameraButton.rx_tap
-         .map{ _ in ImagePickerConfiguration(soruceType: .Camera, allowsEditing: false)}
-         .bindTo(showImagePickerObserver)
-         .addDisposableTo(disposeBag)
-      
-      galleryButton.rx_tap
-         .map{ _ in ImagePickerConfiguration(soruceType: .PhotoLibrary, allowsEditing: false)}
-         .bindTo(showImagePickerObserver)
-         .addDisposableTo(disposeBag)
-      
-      cropButton.rx_tap
-         .map({_ in ImagePickerConfiguration(soruceType: .PhotoLibrary, allowsEditing: true)})
-         .bindTo(showImagePickerObserver)
-         .addDisposableTo(disposeBag)
-      
-      let dismissPickerTrigger = Observable
-         .of(imagePickerController.rx_didFinishPickingMediaWithInfo.map({_ in ()}),
-            imagePickerController.rx_didCancel)
-         .merge()
-      
-      dismissPickerTrigger
-         .subscribeNext { [weak self] in
-            self?.imagePickerController.dismissViewControllerAnimated(true, completion: nil)
-         }
-         .addDisposableTo(disposeBag)
-   }
-   
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var cameraButton: UIButton!
+    @IBOutlet var galleryButton: UIButton!
+    @IBOutlet var cropButton: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+        cameraButton.enabled = UIImagePickerController.isSourceTypeAvailable(.Camera)
+
+        cameraButton.rx_tap
+            .flatMapLatest { [weak self] _ in
+                return UIImagePickerController.rx_createWithParent(self) { picker in
+                    picker.sourceType = .Camera
+                    picker.allowsEditing = false
+                }
+                .flatMap { $0.rx_didFinishPickingMediaWithInfo }
+                .take(1)
+            }
+            .map { info in
+                return info[UIImagePickerControllerOriginalImage] as? UIImage
+            }
+            .bindTo(imageView.rx_image)
+            .addDisposableTo(disposeBag)
+
+        galleryButton.rx_tap
+            .flatMapLatest { [weak self] _ in
+                return UIImagePickerController.rx_createWithParent(self) { picker in
+                    picker.sourceType = .PhotoLibrary
+                    picker.allowsEditing = false
+                }
+                .flatMap {
+                    $0.rx_didFinishPickingMediaWithInfo
+                }
+                .take(1)
+            }
+            .map { info in
+                return info[UIImagePickerControllerOriginalImage] as? UIImage
+            }
+            .bindTo(imageView.rx_image)
+            .addDisposableTo(disposeBag)
+
+        cropButton.rx_tap
+            .flatMapLatest { [weak self] _ in
+                return UIImagePickerController.rx_createWithParent(self) { picker in
+                    picker.sourceType = .PhotoLibrary
+                    picker.allowsEditing = true
+                }
+                .flatMap { $0.rx_didFinishPickingMediaWithInfo }
+                .take(1)
+            }
+            .map { info in
+                return info[UIImagePickerControllerOriginalImage] as? UIImage
+            }
+            .bindTo(imageView.rx_image)
+            .addDisposableTo(disposeBag)
+    }
+    
 }
---
+++ RxExample/RxExample/Examples/ImagePicker/UIImagePickerController+Rx.swift
@@ -0,0 +1,55 @@
+//
+//  UIImagePickerController+Rx.swift
+//  RxExample
+//
+//  Created by Krunoslav Zaher on 1/10/16.
+//  Copyright © 2016 Krunoslav Zaher. All rights reserved.
+//
+
+import Foundation
+import UIKit
+#if !RX_NO_MODULE
+    import RxSwift
+    import RxCocoa
+#endif
+
+func dismissViewController(viewController: UIViewController, animated: Bool) {
+    if viewController.isBeingDismissed() || viewController.isBeingPresented() {
+        dispatch_async(dispatch_get_main_queue()) {
+            dismissViewController(viewController, animated: animated)
+        }
+
+        return
+    }
+
+    if viewController.presentingViewController != nil {
+        viewController.dismissViewControllerAnimated(animated, completion: nil)
+    }
+}
+
+extension UIImagePickerController {
+    static func rx_createWithParent(parent: UIViewController?, animated: Bool = true, configureImagePicker: (UIImagePickerController) throws -> () = { x in }) -> Observable<UIImagePickerController> {
+        return Observable.create { [weak parent] observer in
+            let imagePicker = UIImagePickerController()
+            do {
+                try configureImagePicker(imagePicker)
+            }
+            catch let error {
+                observer.on(.Error(error))
+                return NopDisposable.instance
+            }
+
+            guard let parent = parent else {
+                observer.on(.Completed)
+                return NopDisposable.instance
+            }
+
+            parent.presentViewController(imagePicker, animated: animated, completion: nil)
+            observer.on(.Next(imagePicker))
+
+            return AnonymousDisposable {
+                dismissViewController(imagePicker, animated: animated)
+            }
+        }
+    }
+}
