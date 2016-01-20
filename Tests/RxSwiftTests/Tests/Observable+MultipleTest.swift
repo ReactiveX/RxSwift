2de9976c228cfd740c2bdd2add9de2bc60a59585
Jorge Bernal <jbernal@gmail.com>
2016-01-20 18:02:01 +0100

    Add ForwardIf operator
    ForwardIf propagates the source observable sequence while the condition
    observable sequence last value is true.
    
    This is useful when an Observable performs side effects on subscription, so we
    only want to subscribe when another observable is true.
    
    An example case for this would be an Observable that performs network requests.
    We only want it to hit the network if the subscribing view controller is
    visible.

--- Tests/RxSwiftTests/Tests/Observable+MultipleTest.swift
+++ Tests/RxSwiftTests/Tests/Observable+MultipleTest.swift
@@ -4622,3 +4622,84 @@ extension ObservableMultipleTest {
             ])
     }
 }
+
+// MARK: forwardIf
+extension ObservableMultipleTest {
+    func testForwardIf_simple1() {
+        let scheduler = TestScheduler(initialClock: 0)
+        
+        let xs = scheduler.createHotObservable([
+            next(90, 1),
+            next(180, 2),
+            next(250, 3),
+            next(260, 4),
+            next(310, 5),
+            next(360, 6),
+            completed(390)
+        ])
+        
+        let ys = scheduler.createHotObservable([
+            next(210, true),
+            next(300, false),
+            next(350, true),
+            completed(400)
+        ])
+
+        let res = scheduler.start {
+            xs.forwardIf(ys)
+        }
+
+        XCTAssertEqual(res.events, [
+            next(250, 3),
+            next(260, 4),
+            next(360, 6),
+            completed(390)
+            ])
+
+        XCTAssertEqual(xs.subscriptions, [
+            Subscription(210, 300),
+            Subscription(350, 390)
+            ])
+
+        XCTAssertEqual(ys.subscriptions, [
+            Subscription(200, 390)
+            ])
+
+    }
+
+    func testForwardIf_ConditionCompletedCausesDisposal() {
+        let scheduler = TestScheduler(initialClock: 0)
+
+        let xs = scheduler.createHotObservable([
+            next(90, 1),
+            next(180, 2),
+            next(250, 3),
+            next(260, 4),
+            next(310, 5),
+            next(360, 6),
+            completed(390)
+        ])
+        
+        let ys = scheduler.createHotObservable([
+            next(290, true),
+            completed(300)
+        ])
+
+        let res = scheduler.start {
+            xs.forwardIf(ys)
+        }
+
+        XCTAssertEqual(res.events, [
+            completed(300)
+            ])
+
+        XCTAssertEqual(xs.subscriptions, [
+            Subscription(290, 300)
+            ])
+
+        XCTAssertEqual(ys.subscriptions, [
+            Subscription(200, 300)
+            ])
+
+    }
+}
