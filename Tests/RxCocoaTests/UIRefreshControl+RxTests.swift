974bba5218fa11bc54619d2873759b8870d11d1c
Yosuke Ishikawa <y@ishkawa.org>
2016-01-31 00:33:36 +0900

    Add rx_refreshing for UIRefreshControl

---
+++ Tests/RxCocoaTests/UIRefreshControl+RxTests.swift
@@ -0,0 +1,33 @@
+//
+//  UIRefreshControl+RxTests.swift
+//  RxTests
+//
+//  Created by Yosuke Ishikawa on 1/31/16.
+//  Copyright © 2016 Krunoslav Zaher. All rights reserved.
+//
+
+import Foundation
+
+import RxSwift
+import RxCocoa
+import UIKit
+import XCTest
+
+class UIRefreshControlTests : RxTest {
+}
+
+extension UIRefreshControlTests {
+    func testRefreshing_True() {
+        let subject = UIRefreshControl()
+        Observable.just(true).subscribe(subject.rx_refreshing).dispose()
+
+        XCTAssertTrue(subject.refreshing == true)
+    }
+
+    func testRefreshing_False() {
+        let subject = UIRefreshControl()
+        Observable.just(false).subscribe(subject.rx_refreshing).dispose()
+
+        XCTAssertTrue(subject.refreshing == false)
+    }
+}
