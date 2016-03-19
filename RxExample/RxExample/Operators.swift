8e9ea0349505dd2782dbf1e0e1b91ade40e934f9
Justin Swart <jwswart@gmail.com>
2016-03-19 11:21:09 +0200

    Expanded on example operator to demonstrate how one could create a two-way binding between dissimilar types

--- RxExample/RxExample/Operators.swift
+++ RxExample/RxExample/Operators.swift
@@ -17,6 +17,8 @@ import RxCocoa
 infix operator <-> {
 }
 
+// Usage: textField.rx_text <-> viewModel.variable
+
 func <-> <T>(property: ControlProperty<T>, variable: Variable<T>) -> Disposable {
     let bindToUIDisposable = variable.asObservable()
         .bindTo(property)
@@ -30,5 +32,37 @@ func <-> <T>(property: ControlProperty<T>, variable: Variable<T>) -> Disposable 
     return StableCompositeDisposable.create(bindToUIDisposable, bindToVariable)
 }
 
-// }
+/*  Two way binding of dissimilar types with tuples
+    Usage binding to optional String variable:
 
+    (textField.rx_text, { (text) -> String in
+        return text
+    })
+    <->
+    (viewModel.variable, { (optionalValue) -> String in
+        if let value = optionalValue {
+            return value
+        }
+        
+        return ""
+    })
+*/
+
+func <-> <T, E>(controlTuple: (property: ControlProperty<T>, map: (T -> E)), variableTuple: (variable: Variable<E>, map: (E -> T))) -> RxSwift.Disposable {
+    
+    let (property, propertyMapClosure) = controlTuple
+    let (variable, variableMapClosure) = variableTuple
+    
+    let bindToUIDisposable = variable.asObservable().map(variableMapClosure)
+        .bindTo(property)
+    let bindToVariable = property
+        .map(propertyMapClosure).subscribe(onNext: { n in
+            variable.value = n
+            }, onCompleted:  {
+                bindToUIDisposable.dispose()
+        })
+    
+    return StableCompositeDisposable.create(bindToUIDisposable, bindToVariable)
+}
+
+// }
