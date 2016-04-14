2c7a403daeb3405d73525f1535f3b61733cc3359
Francesco Puntillo <francesco.puntillo@ef.com>
2016-04-14 17:14:20 +0800

    adjust formatting for UIPageControl+RX

--- RxCocoa/iOS/UIPageControl+Rx.swift
+++ RxCocoa/iOS/UIPageControl+Rx.swift
@@ -5,25 +5,25 @@
 //  Created by Francesco Puntillo on 14/04/2016.
 //  Copyright © 2016 Krunoslav Zaher. All rights reserved.
 //
+
 #if os(iOS) || os(tvOS)
     
-    import Foundation
+import Foundation
 #if !RX_NO_MODULE
-    import RxSwift
+import RxSwift
 #endif
-    import UIKit
+import UIKit
     
-    extension UIPageControl {
-        
-        /**
-         Bindable sink for `currentPage` property.
-         */
-        public var rx_currentPage: AnyObserver<Int> {
-            return UIBindingObserver(UIElement: self) { controller, page in
-                controller.currentPage = page
-                }.asObserver()
-        }
-        
+extension UIPageControl {
+    
+    /**
+    Bindable sink for `currentPage` property.
+    */
+    public var rx_currentPage: AnyObserver<Int> {
+        return UIBindingObserver(UIElement: self) { controller, page in
+            controller.currentPage = page
+        }.asObserver()
     }
+}
     
 #endif
