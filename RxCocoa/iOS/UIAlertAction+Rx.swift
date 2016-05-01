2c8bdd819f7e7dc54865217ffcea5028120426af
Andrew Breckenridge <asbreckenridge@me.com>
2016-04-30 23:15:31 -0700

    Create UIAlertAction+Rx.swift

---
+++ RxCocoa/iOS/UIAlertAction+Rx.swift
@@ -0,0 +1,29 @@
+//
+//  UIActivityIndicatorView+Rx.swift
+//  Rx
+//
+//  Created by Andrew Breckenridge on 04/30/15.
+//  Copyright © 2015 Krunoslav Zaher. All rights reserved.
+//
+
+#if os(iOS) || os(tvOS)
+import UIKit
+
+#if !RX_NO_MODULE
+import RxSwift
+#endif
+
+extension UIAlertAction {
+
+  /**
+    Bindable sink for `enabled` property.
+    */
+  public var rx_enabled: AnyObserver<Bool> {
+    return UIBindingObserver(UIElement: self) { alertAction, value in
+      alertAction.enabled = value
+    }.asObserver()
+  }
+
+}
+
+#endif
