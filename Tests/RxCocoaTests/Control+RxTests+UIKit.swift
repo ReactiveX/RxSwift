af7c011a533d617cab1d7e714d5cfd1d7918e3f2
Andrew Breckenridge <asbreckenridge@me.com>
2016-05-08 15:08:43 -0700

    add forgotten parentheses to test function declarations

--- Tests/RxCocoaTests/Control+RxTests+UIKit.swift
+++ Tests/RxCocoaTests/Control+RxTests+UIKit.swift
@@ -161,14 +161,14 @@ extension ControlTests {
 
 // UIAlertAction
 extension ControlTests {
-    func testAlertAction_Enable {
+    func testAlertAction_Enable() {
         let subject = UIAlertAction()
         Observable.just(false).subscribe(subject.rx_enabled).dispose()
         
         XCTAssertTrue(subject.enabled == true)
     }
 
-    func testAlertAction_Disable {
+    func testAlertAction_Disable() {
         let subject = UIAlertAction()
         Observable.just(false).subscribe(subject.rx_enabled).dispose()
         
