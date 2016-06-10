56e6d09b0fb06b233eaa0183d714aef64c0ff794
Jesse Farless <solidcell@gmail.com>
2016-06-10 14:51:40 +0200

    Add a script that updates RxSwift jazzy docs

---
+++ scripts/update-jazzy-docs.sh
@@ -0,0 +1,22 @@
+. scripts/common.sh
+
+function updateDocs() {
+  WORKSPACE=$1
+  SCHEME=$2
+  CONFIGURATION=$3
+  SIMULATOR=$4
+  MODULE=$5
+
+  ensure_simulator_available "${SIMULATOR}"
+  SIMULATOR_GUID=`simulator_ids "${SIMULATOR}"`
+  DESTINATION='id='$SIMULATOR_GUID''
+
+  set -x
+  killall Simulator || true
+  jazzy --config .jazzy.yml -m "${MODULE}" -x -workspace,"${WORKSPACE}",-scheme,"${SCHEME}",-configuration,"${CONFIGURATION}",-derivedDataPath,"${BUILD_DIRECTORY}",-destination,"$DESTINATION"
+  set +x
+}
+
+./scripts/update-jazzy-config.rb
+
+updateDocs Rx.xcworkspace "RxExample-iOS" "Release" $DEFAULT_IOS9_SIMULATOR "RxSwift"
