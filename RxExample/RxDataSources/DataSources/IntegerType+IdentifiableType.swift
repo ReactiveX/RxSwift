bce72a2617aeca6a073087cdda22737d47d2b8d2
Krunoslav Zaher <krunoslav.zaher@gmail.com>
2016-07-04 02:54:32 +0200

    Fixes headers.

--- RxExample/RxDataSources/DataSources/IntegerType+IdentifiableType.swift
+++ RxExample/RxDataSources/DataSources/IntegerType+IdentifiableType.swift
@@ -3,7 +3,7 @@
 //  RxDataSources
 //
 //  Created by Krunoslav Zaher on 7/4/16.
-//  Copyright © 2016 kzaher. All rights reserved.
+//  Copyright © 2016 Krunoslav Zaher. All rights reserved.
 //
 
 import Foundation
