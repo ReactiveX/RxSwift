dbc635d6e7a873720b275d85a962bbf2516023ae
Mo Ramezanpoor <me@mohsenr.com>
2016-08-02 14:01:12 +0100

    Create Disposables type.

---
+++ RxSwift/Disposables/Disposables.swift
@@ -0,0 +1,48 @@
+//
+//  Disposables.swift
+//  Rx
+//
+//  Created by Mohsen Ramezanpoor on 01/08/2016.
+//  Copyright © 2016 Mohsen Ramezanpoor. All rights reserved.
+//
+
+import Foundation
+
+public struct Disposables {
+    
+}
+
+public extension Disposables {
+    
+    static let empty: Disposable = NopDisposable.instance
+    
+    static func create(with action: () -> ()) -> Disposable {
+        return AnonymousDisposable(action)
+    }
+    
+    static func create(_ disposable1: Disposable, _ disposable2: Disposable) -> Disposable {
+        return BinaryDisposable(disposable1, disposable2)
+    }
+    
+    static func create(_ disposable1: Disposable, _ disposable2: Disposable, _ disposable3: Disposable) -> Disposable {
+        return CompositeDisposable(disposable1, disposable2, disposable3)
+    }
+    
+    static func create(_ disposable1: Disposable, _ disposable2: Disposable, _ disposable3: Disposable, _ disposable4: Disposable) -> Disposable {
+        return CompositeDisposable(disposable1, disposable2, disposable3, disposable4)
+    }
+    
+    static func create(_ disposables: [Disposable]) -> Disposable {
+        switch disposables.count {
+        case 0:
+            return Disposables.empty
+        case 1:
+            return disposables[0]
+        case 2:
+            return BinaryDisposable(disposables[0], disposables[1])
+        default:
+            return CompositeDisposable(disposables: disposables)
+        }
+    }
+    
+}
