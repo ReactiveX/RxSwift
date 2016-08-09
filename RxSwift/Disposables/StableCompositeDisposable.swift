2df8c4307790226bc4aa646f6e7e98181d55c063
Mo Ramezanpoor <me@mohsenr.com>
2016-08-09 10:45:29 +0100

    Deprecates `StableCompositeDisposable.create()` in favour of `Disposables.create()`.

--- RxSwift/Disposables/StableCompositeDisposable.swift
+++ RxSwift/Disposables/StableCompositeDisposable.swift
@@ -9,6 +9,7 @@
 import Foundation
 
 public final class StableCompositeDisposable {
+    @available(*, deprecated, renamed: "Disposables.create")
     public static func create(_ disposable1: Disposable, _ disposable2: Disposable) -> Disposable {
         return Disposables.create(disposable1, disposable2)
     }
