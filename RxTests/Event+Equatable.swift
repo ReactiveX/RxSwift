76872b418b3298dc5587487e6265c1ecda4af7c4
sergdort <sergdort@gmail.com>
2016-08-19 14:39:03 +0300

    Added additional overload for `XCTAssertEqual` for `[Recorded<Event<[T]>>]` Added `func == <T: Equatable>(lhs: Recorded<Event<[T]>>, rhs: Recorded<Event<[T]>>) -> Bool` Added `func == <Element: Equatable>(lhs: Event<[Element]>, rhs: Event<[Element]>) -> Bool`

--- RxTests/Event+Equatable.swift
+++ RxTests/Event+Equatable.swift
@@ -40,3 +40,30 @@ public func == <Element: Equatable>(lhs: Event<Element>, rhs: Event<Element>) ->
     default: return false
     }
 }
+
+func == <Element: Equatable>(lhs: Event<[Element]>, rhs: Event<[Element]>) -> Bool {
+    switch (lhs, rhs) {
+    case (.Completed, .Completed): return true
+    case (.Error(let e1), .Error(let e2)):
+        // if the references are equal, then it's the same object
+        if let  lhsObject = lhs as? AnyObject,
+            rhsObject = rhs as? AnyObject
+            where lhsObject === rhsObject {
+            return true
+        }
+        
+        #if os(Linux)
+            return  "\(e1)" == "\(e2)"
+        #else
+            let error1 = e1 as NSError
+            let error2 = e2 as NSError
+            
+            return error1.domain == error2.domain
+                && error1.code == error2.code
+                && "\(e1)" == "\(e2)"
+        #endif
+    case (.Next(let v1), .Next(let v2)): return v1 == v2
+    default: return false
+    }
+}
+
