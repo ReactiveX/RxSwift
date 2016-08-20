c9742e51b6f27eb5f836410d5f208ad7d34b2c98
Krunoslav Zaher <krunoslav.zaher@gmail.com>
2016-08-20 03:13:41 +0200

    Removes Float80 extensions.

--- RxExample/RxDataSources/DataSources/FloatingPointType+IdentifiableType.swift
+++ RxExample/RxDataSources/DataSources/FloatingPointType+IdentifiableType.swift
@@ -23,13 +23,3 @@ extension Float : IdentifiableType {
 extension Double : IdentifiableType {
 
 }
-
-#if swift(>=3.0)
-extension Float80 : IdentifiableType {
-    typealias identity = Float80
-
-    public var identity: Float80 {
-        return self
-    }
-}
-#endif
