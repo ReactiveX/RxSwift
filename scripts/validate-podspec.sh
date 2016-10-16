a4546cfad6c32e41cf35a7631930d9b84aba5db7
Krunoslav Zaher <krunoslav.zaher@gmail.com>
2016-10-16 23:38:30 +0200

    Workarounds because of CocoaPods meets Swift 3.0

--- scripts/validate-podspec.sh
+++ scripts/validate-podspec.sh
@@ -38,7 +38,8 @@ done
 function validate() {
     local PODSPEC=$1
 
-    ~/Projects/CocoaPods/bin/pod lib lint $PODSPEC --verbose --no-clean --allow-warnings # temporary allow warning because of deprecated API in rc
+    # custom cocoapods installation is because of CocoaPods incompatiblity with Swift 3
+    cd ~/.cocoapods/repos/master/; ~/Projects/CocoaPods/bin/pod lib lint ~/Projects/RxSwift/${PODSPEC} --verbose --no-clean --allow-warnings # temporary allow warning because of deprecated API in rc
     #pod lib lint $PODSPEC --verbose --no-clean --allow-warnings # temporary allow warning because of deprecated API in rc
 }
 
