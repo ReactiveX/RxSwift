cae1fc1b1a28e00c0fc0460dc43229b7977154bf
Krunoslav Zaher <krunoslav.zaher@gmail.com>
2016-10-16 19:31:59 +0200

    Removes jazzy gem check from `update-jazzy-config.rb`.

--- scripts/update-jazzy-config.rb
+++ scripts/update-jazzy-config.rb
@@ -2,12 +2,6 @@
 
 require 'yaml'
 
-jazzy_dep = Gem::Dependency.new('jazzy')
-found_jazzy = jazzy_dep.matching_specs
-if found_jazzy.empty?
-  Kernel.abort('jazzy gem not found. Please install it and then try again.')
-end
-
 included_directories = %w(RxSwift RxCocoa)
 
 files_and_directories = included_directories.collect do |directory|
