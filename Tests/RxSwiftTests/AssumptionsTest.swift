59e096d027a1632b567803dd2a98eb32f8066040
Krunoslav Zaher <krunoslav.zaher@gmail.com>
2016-10-30 10:22:07 +0100

    Fixes warning.

--- Tests/RxSwiftTests/AssumptionsTest.swift
+++ Tests/RxSwiftTests/AssumptionsTest.swift
@@ -42,6 +42,7 @@ class AssumptionsTest : RxTest {
         // first check is dealloc method working
         
         var a: Anything? = Anything()
+        _ = a
         XCTAssertFalse(deallocated)
         a = nil
         XCTAssertTrue(deallocated)
