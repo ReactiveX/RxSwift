c4f9c57f734d3f08cbae9da89e195c60d5825f24
Krunoslav Zaher <krunoslav.zaher@gmail.com>
2017-01-15 22:22:45 +0100

    Adds Benchmarks.

---
+++ Tests/Microoptimizations/main.swift
@@ -0,0 +1,37 @@
+//
+//  main.swift
+//  Tests
+//
+//  Created by Krunoslav Zaher on 9/26/15.
+//  Copyright © 2015 Krunoslav Zaher. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+#if !SWIFT_PACKAGE
+import RxCocoa
+#endif
+import AppKit
+import CoreLocation
+
+let bechmarkTime = true
+
+func allocation() {
+    
+}
+
+repeat {
+    compareTwoImplementations(benchmarkTime: true, benchmarkMemory: false, first: {
+        let lock = NSRecursiveLock()
+        for i in 0 ..< 1000 {
+            lock.lock()
+            lock.unlock()
+        }
+    }, second: {
+        let lock = NSRecursiveLock2()
+        for i in 0 ..< 1000 {
+            lock.lock()
+            lock.unlock()
+        }
+    })
+} while true
