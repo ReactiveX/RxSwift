0c67ac8052bacfecae3fe80b3e8d5453d7e452f6
Krunoslav Zaher <krunoslav.zaher@gmail.com>
2017-01-22 18:54:51 +0100

    Adds additional deadlock tests.

--- Tests/RxSwiftTests/Anomalies.swift
+++ Tests/RxSwiftTests/Anomalies.swift
@@ -21,36 +21,38 @@ class AnomaliesTest: RxTest {
 
 extension AnomaliesTest {
     func test936() {
-        func testSharingOperators(share: (Observable<Int>) -> Observable<Int>) {
-            let expectation = self.expectation(description: "wait until sequence completes")
-
+        func performSharingOperatorsTest(share: @escaping (Observable<Int>) -> Observable<Int>) {
             let queue = DispatchQueue(
                 label: "Test",
                 attributes: .concurrent // commenting this to use a serial queue remove the issue
             )
 
-            let scheduler: SchedulerType = ConcurrentDispatchQueueScheduler(queue: queue)
+            for i in 0 ..< 10 {
+                let expectation = self.expectation(description: "wait until sequence completes")
 
-            func makeSequence(label: String, period: RxTimeInterval) -> Observable<Int> {
-                return Observable<Int>
-                    .interval(period, scheduler: scheduler)
-                    .shareReplay(1)
+                queue.async {
+                    let scheduler: SchedulerType = ConcurrentDispatchQueueScheduler(queue: queue, leeway: .milliseconds(5))
+
+                    func makeSequence(label: String, period: RxTimeInterval) -> Observable<Int> {
+                        return share(Observable<Int>.interval(period, scheduler: scheduler))
+                    }
+
+                    let _ = makeSequence(label: "main", period: 0.1)
+                        .flatMapLatest { (index: Int) -> Observable<(Int, Int)> in
+                            return makeSequence(label: "nested", period: 0.02).map { (index, $0) }
+                        }
+                        .take(10)
+                        .mapWithIndex { ($1, $0.0, $0.1) }
+                        .subscribe(
+                            onNext: { _ in },
+                            onCompleted: {
+                                expectation.fulfill()
+                            } 
+                    )
+                }
             }
 
-            let _ = makeSequence(label: "main", period: 0.04)
-                .flatMapLatest { (index: Int) -> Observable<(Int, Int)> in
-                    return makeSequence(label: "nested", period: 0.02).map { (index, $0) }
-                }
-                .take(50)
-                .mapWithIndex { ($1, $0.0, $0.1) }
-                .subscribe(
-                    onNext: { _ in },
-                    onCompleted: {
-                        expectation.fulfill()
-                    } 
-            )
-            
-            waitForExpectations(timeout: 20.0) { (e) in
+            waitForExpectations(timeout: 10.0) { (e) in
                 XCTAssertNil(e)
             }
         }
@@ -58,9 +60,63 @@ extension AnomaliesTest {
         for op in [
                 { $0.shareReplay(1) },
                 { $0.replay(1).refCount() },
+                { $0.publish().refCount() },
                 { $0.shareReplayLatestWhileConnected() }
             ] as [(Observable<Int>) -> Observable<Int>] {
-            testSharingOperators(share: op)
+            performSharingOperatorsTest(share: op)
+        }
+    }
+
+    func testSeparationBetweenOnAndSubscriptionLocks() {
+        func performSharingOperatorsTest(share: @escaping (Observable<Int>) -> Observable<Int>) {
+            for i in 0 ..< 1 {
+                let expectation = self.expectation(description: "wait until sequence completes")
+
+                let queue = DispatchQueue(
+                            label: "off main thread",
+                            attributes: .concurrent
+                        )
+
+                queue.async {
+                    func makeSequence(label: String, period: RxTimeInterval) -> Observable<Int> {
+                        let schedulerQueue = DispatchQueue(
+                            label: "Test",
+                            attributes: .concurrent
+                        )
+
+                        let scheduler: SchedulerType = ConcurrentDispatchQueueScheduler(queue: schedulerQueue, leeway: .milliseconds(0))
+
+                        return share(Observable<Int>.interval(period, scheduler: scheduler))
+                    }
+
+                    let _ = Observable.of(
+                            makeSequence(label: "main", period: 0.2),
+                            makeSequence(label: "nested", period: 0.3)
+                        ).merge()
+                        .take(1)
+                        .subscribe(
+                            onNext: { _ in
+                                Thread.sleep(forTimeInterval: 0.4)
+                            },
+                            onCompleted: {
+                                expectation.fulfill()
+                        }
+                    )
+                }
+            }
+
+            waitForExpectations(timeout: 2.0) { (e) in
+                XCTAssertNil(e)
+            }
+        }
+
+        for op in [
+            { $0.shareReplay(1) },
+            { $0.replay(1).refCount() },
+            { $0.publish().refCount() },
+            { $0.shareReplayLatestWhileConnected() }
+            ] as [(Observable<Int>) -> Observable<Int>] {
+            performSharingOperatorsTest(share: op)
         }
     }
 }
