aee5c28f1a814c0eceadc372d2ef8d7854bfca7c
Yasuhiro Inami <inamiy@gmail.com>
2017-03-17 21:40:37 +0900

    Add `Event.isCompleted`

--- RxSwift/Event.swift
+++ RxSwift/Event.swift
@@ -36,7 +36,7 @@ extension Event : CustomDebugStringConvertible {
 }
 
 extension Event {
-    /// Is `Completed` or `Error` event.
+    /// Is `completed` or `error` event.
     public var isStopEvent: Bool {
         switch self {
         case .next: return false
@@ -44,7 +44,7 @@ extension Event {
         }
     }
 
-    /// If `Next` event, returns element value.
+    /// If `next` event, returns element value.
     public var element: Element? {
         if case .next(let value) = self {
             return value
@@ -52,12 +52,20 @@ extension Event {
         return nil
     }
 
-    /// If `Error` event, returns error.
+    /// If `error` event, returns error.
     public var error: Swift.Error? {
         if case .error(let error) = self {
             return error
         }
         return nil
+    }
+
+    /// If `completed` event, returns true.
+    public var isCompleted: Bool {
+        if case .completed = self {
+            return true
+        }
+        return false
     }
 }
 
