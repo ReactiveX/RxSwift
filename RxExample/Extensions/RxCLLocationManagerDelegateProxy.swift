2308e452e1d8959f496638743a231159d0de1c68
phlippieb <phlippie.bosman@gmail.com>
2017-04-20 14:40:47 +0200

    Fix CLLocationManager extension example

--- RxExample/Extensions/RxCLLocationManagerDelegateProxy.swift
+++ RxExample/Extensions/RxCLLocationManagerDelegateProxy.swift
@@ -15,14 +15,29 @@ import CoreLocation
 class RxCLLocationManagerDelegateProxy : DelegateProxy
                                        , CLLocationManagerDelegate
                                        , DelegateProxyType {
-    
+
+    internal lazy var didUpdateLocationsSubject = PublishSubject<[CLLocation]>()
+    internal lazy var didFailWithErrorSubject = PublishSubject<Error>()
+
     class func currentDelegateFor(_ object: AnyObject) -> AnyObject? {
         let locationManager: CLLocationManager = object as! CLLocationManager
         return locationManager.delegate
     }
-    
+
     class func setCurrentDelegate(_ delegate: AnyObject?, toObject object: AnyObject) {
         let locationManager: CLLocationManager = object as! CLLocationManager
-        locationManager.delegate = delegate as? CLLocationManagerDelegate
+        if let delegate = delegate {
+            locationManager.delegate = (delegate as! CLLocationManagerDelegate)
+        } else {
+            locationManager.delegate = nil
+        }
+    }
+
+    public func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        didUpdateLocationsSubject.onNext(locations)
+    }
+
+    public func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        didFailWithErrorSubject.onNext(error)
     }
 }
