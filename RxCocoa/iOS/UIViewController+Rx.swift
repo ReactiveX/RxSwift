ad6739ade4378a0263132e2c9bf3c353dc490a6c
muukii <m@muukii.me>
2017-05-17 17:09:18 +0900

    Add ViewController LifeCycle

--- RxCocoa/iOS/UIViewController+Rx.swift
+++ RxCocoa/iOS/UIViewController+Rx.swift
@@ -14,13 +14,40 @@
 #endif
 
     extension Reactive where Base: UIViewController {
-
+        
         /// Bindable sink for `title`.
         public var title: UIBindingObserver<Base, String> {
             return UIBindingObserver(UIElement: self.base) { viewController, title in
                 viewController.title = title
             }
         }
-    
+        
+        public var viewDidLoad: ControlEvent<Void> {
+            return ControlEvent(events: sentMessage(#selector(UIViewController.viewDidLoad)).map { _ in })
+        }
+        
+        public var viewWillAppear: ControlEvent<Void> {
+            return ControlEvent(events: sentMessage(#selector(UIViewController.viewWillAppear(_:))).map { _ in })
+        }
+        
+        public var viewDidAppear: ControlEvent<Void> {
+            return ControlEvent(events: sentMessage(#selector(UIViewController.viewDidAppear(_:))).map { _ in })
+        }
+        
+        public var viewWillDisappear: ControlEvent<Void> {
+            return ControlEvent(events: sentMessage(#selector(UIViewController.viewWillDisappear(_:))).map { _ in })
+        }
+        
+        public var viewDidDisapppear: ControlEvent<Void> {
+            return ControlEvent(events: sentMessage(#selector(UIViewController.viewDidDisappear(_:))).map { _ in })
+        }
+        
+        public var viewWillLayoutSubviews: ControlEvent<Void> {
+            return ControlEvent(events: sentMessage(#selector(UIViewController.viewWillLayoutSubviews)).map { _ in })
+        }
+        
+        public var viewDidLayoutSubviews: ControlEvent<Void> {
+            return ControlEvent(events: sentMessage(#selector(UIViewController.viewDidLayoutSubviews)).map { _ in })
+        }
     }
 #endif
