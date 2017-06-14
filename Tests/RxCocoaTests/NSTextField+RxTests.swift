c0644c2c9bff10f307fcf7ae2740ae2e3d61096b
Christian Tietze <christian.tietze@gmail.com>
2017-06-14 08:32:42 +0200

    add delegate tests

--- Tests/RxCocoaTests/NSTextField+RxTests.swift
+++ Tests/RxCocoaTests/NSTextField+RxTests.swift
@@ -20,4 +20,47 @@ extension NSTextFieldTests {
         let createView: () -> NSTextField = { NSTextField(frame: CGRect(x: 0, y: 0, width: 1, height: 1)) }
         ensurePropertyDeallocated(createView, "a") { (view: NSTextField) in view.rx.text.orEmpty }
     }
+
+    func testTextField_ControlTextDidChange_ForwardsToDelegates() {
+
+        var completed = false
+
+        autoreleasepool {
+            let textField = NSTextField()
+            let delegate = TextFieldDelegate()
+            textField.delegate = delegate
+            var rxDidChange = false
+
+            _ = textField.rx.text
+                .skip(1) // Initial value
+                .subscribe(onNext: { _ in
+                    rxDidChange = true
+                }, onCompleted: {
+                    completed = true
+                })
+
+            XCTAssertFalse(rxDidChange)
+            XCTAssertFalse(delegate.didChange)
+
+            let notification = Notification(
+                name: .NSControlTextDidChange,
+                object: textField,
+                userInfo: ["NSFieldEditor" : NSText()])
+            (textField.delegate as! NSObject).controlTextDidChange(notification)
+
+            XCTAssertTrue(rxDidChange)
+            XCTAssertTrue(delegate.didChange)
+        }
+
+        XCTAssertTrue(completed)
+    }
+
+}
+
+fileprivate final class TextFieldDelegate: NSObject, NSTextFieldDelegate {
+
+    var didChange = false
+    override func controlTextDidChange(_ notification: Notification) {
+        didChange = true
+    }
 }
