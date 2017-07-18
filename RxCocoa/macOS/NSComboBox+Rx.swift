55ba2f2ce0730db95943f5aafe2c8c24753ee208
Jacob Gorban <jacob@apparentsoft.com>
2017-07-18 01:10:49 -0500

    Add NSComboBox tests

---
+++ RxCocoa/macOS/NSComboBox+Rx.swift
@@ -0,0 +1,105 @@
+//
+//  NSComboBox+Rx.swift
+//  RxCocoa-macOS
+//
+//  Created by Jacob Gorban on 2017-07-17.
+//  Copyright © 2017 Krunoslav Zaher. All rights reserved.
+//
+
+#if os(macOS)
+
+import Cocoa
+#if !RX_NO_MODULE
+import RxSwift
+#endif
+    
+/// Delegate proxy for `NSComboBox`.
+///
+/// For more information take a look at `DelegateProxyType`.
+public class RxComboBoxDelegateProxy
+    : DelegateProxy
+    , NSComboBoxDelegate
+    , DelegateProxyType {
+
+    fileprivate let selectionIndexSubject = PublishSubject<Int>()
+
+    /// Typed parent object.
+    public weak private(set) var comboBox: NSComboBox?
+
+    /// Initializes `RxComboBoxDelegateProxy`
+    ///
+    /// - parameter parentObject: Parent object for delegate proxy.
+    public required init(parentObject: AnyObject) {
+        self.comboBox = castOrFatalError(parentObject)
+        super.init(parentObject: parentObject)
+    }
+
+    // MARK: Delegate methods
+
+    public func comboBoxSelectionDidChange(_ notification: Notification) {
+        let comboBox: NSComboBox = castOrFatalError(notification.object)
+        let nextValue = comboBox.indexOfSelectedItem
+        self.selectionIndexSubject.on(.next(nextValue))
+        _forwardToDelegate?.comboBoxSelectionDidChange(notification)
+    }
+
+    // MARK: Delegate proxy methods
+
+    /// For more information take a look at `DelegateProxyType`.
+    public override class func createProxyForObject(_ object: AnyObject) -> AnyObject {
+        let control: NSComboBox = castOrFatalError(object)
+        return control.createComboBoxRxDelegateProxy()
+    }
+
+    /// For more information take a look at `DelegateProxyType`.
+    public class func currentDelegateFor(_ object: AnyObject) -> AnyObject? {
+        let comboBox: NSComboBox = castOrFatalError(object)
+        return comboBox.delegate
+    }
+
+    /// For more information take a look at `DelegateProxyType`.
+    public class func setCurrentDelegate(_ delegate: AnyObject?, toObject object: AnyObject) {
+        let comboBox: NSComboBox = castOrFatalError(object)
+        comboBox.delegate = castOptionalOrFatalError(delegate)
+    }
+}
+
+extension NSComboBox {
+
+    /// Factory method that enables subclasses to implement their own `delegate`.
+    ///
+    /// - returns: Instance of delegate proxy that wraps `delegate`.
+    public func createComboBoxRxDelegateProxy() -> RxComboBoxDelegateProxy {
+        return RxComboBoxDelegateProxy(parentObject: self)
+    }
+}
+
+extension Reactive where Base: NSComboBox {
+
+    /// Reactive wrapper for `delegate`.
+    ///
+    /// For more information take a look at `DelegateProxyType` protocol documentation.
+    public var delegate: DelegateProxy {
+        return RxComboBoxDelegateProxy.proxyForObject(base)
+    }
+
+    /// Reactive wrapper for `indexOfSelectedItem` property.
+    public var indexOfSelectedItem: ControlProperty<Int> {
+        let delegate = RxComboBoxDelegateProxy.proxyForObject(base)
+
+        let source = Observable.deferred { [weak comboBox = self.base] in
+            delegate.selectionIndexSubject.startWith(comboBox?.indexOfSelectedItem ?? -1)
+            }.takeUntil(deallocated)
+
+        let observer = UIBindingObserver(UIElement: base) { (control, value: Int) in
+            guard value >= 0, value < control.numberOfItems else { return }
+            control.selectItem(at: value)
+        }
+
+        return ControlProperty(values: source, valueSink: observer.asObserver())
+    }
+
+}
+
+
+#endif
---
+++ Tests/RxCocoaTests/NSComboBox+RxTests.swift
@@ -0,0 +1,66 @@
+//
+//  NSComboBox+RxTests.swift
+//  AllTests-macOS
+//
+//  Created by Jacob Gorban on 2017-07-17.
+//  Copyright © 2017 Krunoslav Zaher. All rights reserved.
+//
+
+import RxSwift
+import RxCocoa
+import AppKit
+import XCTest
+
+final class NSComboBoxTests: RxTest {
+
+}
+
+extension NSComboBoxTests {
+    func testComboBox_IndexOfSelectedItemCompletesOnDealloc() {
+        let createView: () -> NSComboBox = { NSComboBox(frame: CGRect(x: 0, y: 0, width: 1, height: 1)) }
+        ensurePropertyDeallocated(createView, -1) { (view: NSComboBox) in view.rx.indexOfSelectedItem }
+    }
+
+    func testComboBox_ComboBoxSelectionDidChange_ForwardsToDelegates() {
+
+        var completed = false
+
+        autoreleasepool {
+            let comboBox = NSComboBox()
+            let delegate = ComboBoxDelegate()
+            comboBox.delegate = delegate
+            var rxDidChange = false
+
+            _ = comboBox.rx.indexOfSelectedItem
+                .skip(1) // Initial value
+                .subscribe(onNext: { _ in
+                    rxDidChange = true
+                }, onCompleted: {
+                    completed = true
+                })
+
+            XCTAssertFalse(rxDidChange)
+            XCTAssertFalse(delegate.didChange)
+
+            let notification = Notification(
+                name: .NSComboBoxSelectionDidChange,
+                object: comboBox,
+                userInfo: [:])
+            comboBox.delegate!.comboBoxSelectionDidChange!(notification)
+
+            XCTAssertTrue(rxDidChange)
+            XCTAssertTrue(delegate.didChange)
+        }
+
+        XCTAssertTrue(completed)
+    }
+
+}
+
+fileprivate final class ComboBoxDelegate: NSObject, NSComboBoxDelegate {
+
+    var didChange = false
+    func comboBoxSelectionDidChange(_ notification: Notification) {
+        didChange = true
+    }
+}
