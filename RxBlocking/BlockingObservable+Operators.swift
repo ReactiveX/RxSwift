9a04bafa683d6f1b53886017bb10f8860d0ed234
Stewart Gleadow <sgleadow@gmail.com>
2017-08-17 08:24:58 +1000

    Introduce and use SequenceMaterializeResult type internally - all existing tests still valid and pass

--- RxBlocking/BlockingObservable+Operators.swift
+++ RxBlocking/BlockingObservable+Operators.swift
@@ -10,6 +10,11 @@
     import RxSwift
 #endif
 
+enum SequenceMaterializeResult<T> {
+    case completed(elements: [T])
+    case failed(elements: [T], error: Error)
+}
+
 extension BlockingObservable {
     /// Blocks current thread until sequence terminates.
     ///
@@ -17,7 +22,8 @@ extension BlockingObservable {
     ///
     /// - returns: All elements of sequence.
     public func toArray() throws -> [E] {
-        return try convertToArray()
+        let results = convertToArray()
+        return try elementsOrThrow(results)
     }
 }
 
@@ -28,7 +34,8 @@ extension BlockingObservable {
     ///
     /// - returns: First element of sequence. If sequence is empty `nil` is returned.
     public func first() throws -> E? {
-        return try convertToArray(max: 1).first
+        let results = convertToArray(max: 1)
+        return try elementsOrThrow(results).first
     }
 }
 
@@ -39,7 +46,8 @@ extension BlockingObservable {
     ///
     /// - returns: Last element in the sequence. If sequence is empty `nil` is returned.
     public func last() throws -> E? {
-        return try convertToArray().last
+        let results = convertToArray()
+        return try elementsOrThrow(results).last
     }
 }
 
@@ -60,7 +68,8 @@ extension BlockingObservable {
     /// - parameter predicate: A function to test each source element for a condition.
     /// - returns: Returns the only element of an sequence that satisfies the condition in the predicate, and reports an error if there is not exactly one element in the sequence.
     public func single(_ predicate: @escaping (E) throws -> Bool) throws -> E? {
-        let elements = try convertToArray(max: 2, predicate: predicate)
+        let results = convertToArray(max: 2, predicate: predicate)
+        let elements = try elementsOrThrow(results)
         
         switch elements.count {
         case 0:
@@ -74,9 +83,8 @@ extension BlockingObservable {
 }
 
 extension BlockingObservable {
-    fileprivate func convertToArray(max: Int? = nil, predicate: @escaping (E) throws -> Bool = { _ in true }) throws -> [E] {
+    fileprivate func convertToArray(max: Int? = nil, predicate: @escaping (E) throws -> Bool = { _ in true }) -> SequenceMaterializeResult<E> {
         var elements: [E] = Array<E>()
-        
         var error: Swift.Error?
         
         let lock = RunLoopLock(timeout: timeout)
@@ -127,9 +135,18 @@ extension BlockingObservable {
         }
         
         if let error = error {
-            throw error
+            return SequenceMaterializeResult.failed(elements: elements, error: error)
         }
         
-        return elements
+        return SequenceMaterializeResult.completed(elements: elements)
+    }
+    
+    fileprivate func elementsOrThrow(_ results: SequenceMaterializeResult<E>) throws -> [E] {
+        switch results {
+        case .failed(_, let error):
+            throw error
+        case .completed(let elements):
+            return elements
+        }
     }
 }
