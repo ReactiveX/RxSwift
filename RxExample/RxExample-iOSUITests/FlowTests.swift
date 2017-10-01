981b98a7b68f958da418ddd59fa082d5cf6f4d1c
Krunoslav Zaher <krunoslav.zaher@gmail.com>
2017-10-01 13:44:55 +0200

    Polishes attributedText.

--- RxExample/RxExample-iOSUITests/FlowTests.swift
+++ RxExample/RxExample-iOSUITests/FlowTests.swift
@@ -125,14 +125,14 @@ extension FlowTests {
 extension FlowTests {
     func testControls() {
         for test in [
-        _testDatePicker,
-        _testBarButtonItemTap,
-        _testButtonTap,
-        _testSegmentedControl,
-        _testUISwitch,
-        _testUITextField,
-        _testUITextView,
-        _testSlider
+            _testDatePicker,
+            _testBarButtonItemTap,
+            _testButtonTap,
+            _testSegmentedControl,
+            _testUISwitch,
+            _testUITextField,
+            _testUITextView,
+            _testSlider
             ] {
             goToControlsView()
             test()
@@ -148,9 +148,14 @@ extension FlowTests {
         tableView.cells.allElementsBoundByIndex[5].tap()
     }
 
-    func checkDebugLabelValue(_ expected: String) {
+    func checkDebugLabelValue(_ expected: String, hasPrefix: Bool = false) {
         let textValue = app.staticTexts["debugLabel"].value as? String
-        XCTAssertEqual(textValue, expected)
+        if hasPrefix {
+            XCTAssertTrue((textValue ?? "").hasPrefix(expected))
+        }
+        else {
+            XCTAssertEqual(textValue, expected)
+        }
     }
 
     func _testDatePicker() {
@@ -184,9 +189,9 @@ extension FlowTests {
 
     func _testUISwitch() {
         let switchControl = app.switches.allElementsBoundByIndex[0]
-        switchControl.tap()
+        switchControl.twoFingerTap()
         checkDebugLabelValue("UISwitch value false")
-        switchControl.tap()
+        switchControl.twoFingerTap()
         checkDebugLabelValue("UISwitch value true")
     }
 
@@ -195,6 +200,10 @@ extension FlowTests {
         textField.tap()
         textField.typeText("f")
         checkDebugLabelValue("UITextField text f")
+        let textField2 = app.textFields.allElementsBoundByIndex[1]
+        textField2.tap()
+        textField2.typeText("f2")
+        checkDebugLabelValue("UITextField attributedText f2{", hasPrefix: true)
     }
 
     func _testUITextView() {
@@ -202,12 +211,16 @@ extension FlowTests {
         textView.tap()
         textView.typeText("f")
         checkDebugLabelValue("UITextView text f")
+        let textView2 = app.textViews.allElementsBoundByIndex[1]
+        textView2.tap()
+        textView2.typeText("f2")
+        checkDebugLabelValue("UITextView attributedText f2{", hasPrefix: true)
     }
 
     func _testSlider() {
         let slider = app.sliders.allElementsBoundByIndex[0]
         slider.adjust(toNormalizedSliderPosition: 0)
-        checkDebugLabelValue("UISlider value 0.0")
+        checkDebugLabelValue("UISlider value 0.0", hasPrefix: true)
     }
 }
 
