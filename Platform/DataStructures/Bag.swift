8c1f1de7eefcf62b85952e95f418373ea8de891b
beeth0ven <beeth0vendev@163.com>
2017-10-20 10:30:02 +0800

    Remove unnecessary in Bag

--- Platform/DataStructures/Bag.swift
+++ Platform/DataStructures/Bag.swift
@@ -83,12 +83,8 @@ struct Bag<T> : CustomDebugStringConvertible {
             _pairs.append((key: key, value: element))
             return key
         }
-
-        if _dictionary == nil {
-            _dictionary = [:]
-        }
-
-        _dictionary![key] = element
+        
+        _dictionary = [key: element]
         
         return key
     }
