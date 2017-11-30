b6b3a0dedae715577a047ff7c7a26faa2c6bb7a9
beeth0ven <beeth0vendev@163.com>
2017-11-30 13:13:15 +0800

    Deprecated Recorded Event global factory method

--- RxTest/Deprecated.swift
+++ RxTest/Deprecated.swift
@@ -19,3 +19,43 @@ extension TestScheduler {
         return start(created: created, subscribed: subscribed, disposed: disposed, create: create)
     }
 }
+
+/**
+ These methods are conceptually extensions of `XCTestCase` but because referencing them in closures would
+ require specifying `self.*`, they are made global.
+ */
+//extension XCTestCase {
+    /**
+     Factory method for an `.next` event recorded at a given time with a given value.
+ 
+     - parameter time: Recorded virtual time the `.next` event occurs.
+     - parameter element: Next sequence element.
+     - returns: Recorded event in time.
+     */
+    @available(*, deprecated, renamed: "Recorded.next(_:_:)")
+    public func next<T>(_ time: TestTime, _ element: T) -> Recorded<Event<T>> {
+        return Recorded(time: time, value: .next(element))
+    }
+
+    /**
+     Factory method for an `.completed` event recorded at a given time.
+ 
+     - parameter time: Recorded virtual time the `.completed` event occurs.
+     - parameter type: Sequence elements type.
+     - returns: Recorded event in time.
+     */
+    @available(*, deprecated, renamed: "Recorded.completed(_:_:)")
+    public func completed<T>(_ time: TestTime, _ type: T.Type = T.self) -> Recorded<Event<T>> {
+        return Recorded(time: time, value: .completed)
+    }
+
+    /**
+     Factory method for an `.error` event recorded at a given time with a given error.
+ 
+     - parameter time: Recorded virtual time the `.completed` event occurs.
+     */
+    @available(*, deprecated, renamed: "Recorded.error(_:_:_:)")
+    public func error<T>(_ time: TestTime, _ error: Swift.Error, _ type: T.Type = T.self) -> Recorded<Event<T>> {
+        return Recorded(time: time, value: .error(error))
+    }
+//}
