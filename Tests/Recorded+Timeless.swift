1880499543118afb969c7e1c78bf3ae687f0e194
beeth0ven <beeth0vendev@163.com>
2017-12-26 21:37:35 +0800

    Add Recorded Event timeless factory method

--- Tests/Recorded+Timeless.swift
+++ Tests/Recorded+Timeless.swift
@@ -9,6 +9,21 @@
 import RxTest
 import RxSwift
 
+extension Recorded {
+    
+    public static func next<T>(_ element: T) -> Recorded<Event<T>> where Value == Event<T> {
+        return Recorded(time: 0, value: .next(element))
+    }
+    
+    public static func completed<T>(_ type: T.Type = T.self) -> Recorded<Event<T>> where Value == Event<T> {
+        return Recorded(time: 0, value: .completed)
+    }
+    
+    public static func error<T>(_ error: Swift.Error, _ type: T.Type = T.self) -> Recorded<Event<T>> where Value == Event<T> {
+        return Recorded(time: 0, value: .error(error))
+    }
+}
+
 func next<T>(_ value: T) -> Recorded<Event<T>> {
     return Recorded(time: 0, value: .next(value))
 }
