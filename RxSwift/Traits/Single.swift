199cb4e96cf4f0d1af91b6d933f658615d542f4d
Pietro Caselani <pc1992@gmail.com>
2018-03-19 11:21:34 -0300

    Add operator `flatMapCompletable` on Single trait

--- RxSwift/Traits/Single.swift
+++ RxSwift/Traits/Single.swift
@@ -261,7 +261,20 @@ extension PrimitiveSequenceType where TraitType == SingleTrait {
         -> Maybe<R> {
             return Maybe<R>(raw: primitiveSequence.source.flatMap(selector))
     }
-    
+
+    /**
+     Projects each element of an observable sequence to an observable sequence and merges the resulting observable sequences into one observable sequence.
+
+     - seealso: [flatMap operator on reactivex.io](http://reactivex.io/documentation/operators/flatmap.html)
+
+     - parameter selector: A transform function to apply to each element.
+     - returns: An observable sequence whose elements are the result of invoking the one-to-many transform function on each element of the input sequence.
+     */
+    public func flatMapCompletable(_ selector: @escaping (ElementType) throws -> Completable)
+        -> Completable {
+            return Completable(raw: primitiveSequence.source.flatMap(selector))
+    }
+
     /**
      Merges the specified observable sequences into one observable sequence by using the selector function whenever all of the observable sequences have produced an element at a corresponding index.
      
