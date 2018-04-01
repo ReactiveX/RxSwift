29026c0c66b82ca1b9e30a100f9e5b12a7d48392
Vladimir Kushelkov <vldalxnd@gmail.com>
2018-04-01 03:43:42 +0300

    adding willReposition ControlEvent to UIPopoverPresentationController Reactive extension

--- RxCocoa/iOS/UIPopoverPresentationController+Rx.swift
+++ RxCocoa/iOS/UIPopoverPresentationController+Rx.swift
@@ -13,6 +13,9 @@ import RxSwift
 
 @available(iOS 8.0, *)
 extension Reactive where Base: UIPopoverPresentationController {
+    public typealias WillRepositionPopoverEvent =
+        (toRect: UnsafeMutablePointer<CGRect>, inView: AutoreleasingUnsafeMutablePointer<UIView>)
+    
     public var delegate: DelegateProxy<UIPopoverPresentationController, UIPopoverPresentationControllerDelegate> {
         return RxPopoverPresentationControllerProxy.proxy(for: base)
     }
@@ -27,6 +30,25 @@ extension Reactive where Base: UIPopoverPresentationController {
         return delegate
             .methodInvoked(#selector(UIPopoverPresentationControllerDelegate.prepareForPopoverPresentation(_:)))
             .map {_ in}
+    }
+    
+    public var willReposition: ControlEvent<WillRepositionPopoverEvent> {
+        let source = delegate
+            .methodInvoked(#selector(UIPopoverPresentationControllerDelegate.popoverPresentationController(_:willRepositionPopoverTo:in:)))
+            .map { args -> WillRepositionPopoverEvent in
+                let rect = try castOrThrow(NSValue.self, args[1])
+                let view = try castOrThrow(NSValue.self, args[2])
+                
+                guard let rawRectPointer = rect.pointerValue else { throw RxCocoaError.unknown }
+                let typedRectPointer = rawRectPointer.bindMemory(to: CGRect.self, capacity: MemoryLayout<CGRect>.size)
+                
+                guard let rawViewPointer = view.pointerValue else { throw RxCocoaError.unknown }
+                let typedViewPointer = rawViewPointer.bindMemory(to: UIView.self, capacity: MemoryLayout<UIView>.size)
+                
+                return (typedRectPointer, .init(typedViewPointer))
+            }
+        
+        return ControlEvent(events: source)
     }
     
     public func setDelegate(_ delegate: UIPopoverPresentationControllerDelegate) -> Disposable {
