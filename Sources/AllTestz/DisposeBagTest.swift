0f8db8a4be4c35a5360fe2329caa00365ba6e645
Michael Long <mlong@clientresourcesinc.com>
2018-06-16 08:32:47 -0500

    Add DisposeBag.insert(...) to support multiple Disposables

---
+++ Sources/AllTestz/DisposeBagTest.swift
@@ -0,0 +1,1 @@
+../../Tests/RxSwiftTests/DisposeBagTest.swift
---
+++ Tests/RxSwiftTests/DisposeBagTest.swift
@@ -0,0 +1,69 @@
+//
+//  DisposeBagTest.swift
+//  Tests
+//
+//  Created by Michael Long on 6/16/18.
+//  Copyright © 2018 Krunoslav Zaher. All rights reserved.
+//
+
+import XCTest
+import RxSwift
+import RxTest
+
+class DisposeBagTest : RxTest {
+    override func setUp() {
+        super.setUp()
+    }
+
+    override func tearDown() {
+        super.tearDown()
+    }
+}
+
+// DisposeBag insert test
+extension DisposeBagTest {
+
+    func testDisposeBagInsert() {
+        let disposable1 = TestDisposable()
+        let disposable2 = TestDisposable()
+
+        var disposeBag: DisposeBag? = DisposeBag()
+
+        disposeBag?.insert(disposable1)
+        disposeBag?.insert(disposable2)
+
+        XCTAssert(disposable1.count == 0)
+        XCTAssert(disposable2.count == 0)
+        disposeBag = nil
+        XCTAssert(disposable1.count == 1)
+        XCTAssert(disposable2.count == 1)
+    }
+
+}
+
+// DisposeBag bag test
+extension DisposeBagTest {
+
+    func testDisposeBagVaradicInsert() {
+        let disposable1 = TestDisposable()
+        let disposable2 = TestDisposable()
+
+        var disposeBag: DisposeBag? = DisposeBag()
+
+        disposeBag?.insert(disposable1, disposable2)
+
+        XCTAssert(disposable1.count == 0)
+        XCTAssert(disposable2.count == 0)
+        disposeBag = nil
+        XCTAssert(disposable1.count == 1)
+        XCTAssert(disposable2.count == 1)
+    }
+
+}
+
+fileprivate class TestDisposable: Disposable {
+    var count = 0
+    func dispose() {
+        count += 1
+    }
+}
