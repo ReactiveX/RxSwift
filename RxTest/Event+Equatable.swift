9abca416ed1a918b3b6e62d208df0b9dbd7c8653
Luciano Almeida <passos.luciano@outlook.com>
2018-10-06 01:10:58 -0300

    Put back operator Event with optional Element.

--- RxTest/Event+Equatable.swift
+++ RxTest/Event+Equatable.swift
@@ -28,6 +28,25 @@ internal func equals<Element: Equatable>(lhs: Event<Element>, rhs: Event<Element
     }
 }
 
+internal func equals<Element: Equatable>(lhs: Event<Element?>, rhs: Event<Element?>) -> Bool {
+    switch (lhs, rhs) {
+    case (.completed, .completed): return true
+    case (.error(let e1), .error(let e2)):
+        #if os(Linux)
+        return  "\(e1)" == "\(e2)"
+        #else
+        let error1 = e1 as NSError
+        let error2 = e2 as NSError
+        
+        return error1.domain == error2.domain
+            && error1.code == error2.code
+            && "\(e1)" == "\(e2)"
+        #endif
+    case (.next(let v1), .next(let v2)): return v1 == v2
+    default: return false
+    }
+}
+
 internal func equals<Element: Equatable>(lhs: SingleEvent<Element>, rhs: SingleEvent<Element>) -> Bool {
     switch (lhs, rhs) {
     case (.error(let e1), .error(let e2)):
@@ -107,13 +126,16 @@ extension MaybeEvent: Equatable where Element: Equatable {
         return equals(lhs: lhs, rhs: rhs)
     }
 }
-#else
 
 /// Compares two events. They are equal if they are both the same member of `Event` enumeration.
 ///
 /// In case `Error` events are being compared, they are equal in case their `NSError` representations are equal (domain and code)
 /// and their string representations are equal.
 public func == <Element: Equatable>(lhs: Event<Element>, rhs: Event<Element>) -> Bool {
+    return equals(lhs: lhs, rhs: rhs)
+}
+
+public func == <Element: Equatable>(lhs: Event<Element?>, rhs: Event<Element?>) -> Bool {
     return equals(lhs: lhs, rhs: rhs)
 }
 
@@ -133,4 +155,3 @@ public func == <Element: Equatable>(lhs: MaybeEvent<Element>, rhs: MaybeEvent<El
     return equals(lhs: lhs, rhs: rhs)
 }
 
-#endif
