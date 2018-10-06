c9cc7754e2dae2063a42c8e12eea7fbfb0bd44ed
Luciano Almeida <passos.luciano@outlook.com>
2018-10-05 23:13:22 -0300

    Adding conditional compilation for handle conditional conformance.

--- RxTest/Event+Equatable.swift
+++ RxTest/Event+Equatable.swift
@@ -9,79 +9,84 @@
 import RxSwift
 import class Foundation.NSError
 
+internal func equals<Element: Equatable>(lhs: Event<Element>, rhs: Event<Element>) -> Bool {
+    switch (lhs, rhs) {
+    case (.completed, .completed): return true
+    case (.error(let e1), .error(let e2)):
+        #if os(Linux)
+        return  "\(e1)" == "\(e2)"
+        #else
+        let error1 = e1 as NSError
+        let error2 = e2 as NSError
+        
+        return error1.domain == error2.domain
+            && error1.code == error2.code
+            && "\(e1)" == "\(e2)"
+        #endif
+    case (.next(let v1), .next(let v2)): return v1 == v2
+    default: return false
+    }
+}
+
+internal func equals<Element: Equatable>(lhs: SingleEvent<Element>, rhs: SingleEvent<Element>) -> Bool {
+    switch (lhs, rhs) {
+    case (.error(let e1), .error(let e2)):
+        #if os(Linux)
+        return  "\(e1)" == "\(e2)"
+        #else
+        let error1 = e1 as NSError
+        let error2 = e2 as NSError
+        
+        return error1.domain == error2.domain
+            && error1.code == error2.code
+            && "\(e1)" == "\(e2)"
+        #endif
+    case (.success(let v1), .success(let v2)): return v1 == v2
+    default: return false
+    }
+}
+
+internal func equals<Element: Equatable>(lhs: MaybeEvent<Element>, rhs: MaybeEvent<Element>) -> Bool {
+    switch (lhs, rhs) {
+    case (.completed, .completed): return true
+    case (.error(let e1), .error(let e2)):
+        #if os(Linux)
+        return  "\(e1)" == "\(e2)"
+        #else
+        let error1 = e1 as NSError
+        let error2 = e2 as NSError
+        
+        return error1.domain == error2.domain
+            && error1.code == error2.code
+            && "\(e1)" == "\(e2)"
+        #endif
+    case (.success(let v1), .success(let v2)): return v1 == v2
+    default: return false
+    }
+}
+
 /// Compares two events. They are equal if they are both the same member of `Event` enumeration.
 ///
 /// In case `Error` events are being compared, they are equal in case their `NSError` representations are equal (domain and code)
 /// and their string representations are equal.
-extension Event: Equatable where Element: Equatable {
-    
-    public static func == (lhs: Event<Element>, rhs: Event<Element>) -> Bool {
-        switch (lhs, rhs) {
-        case (.completed, .completed): return true
-        case (.error(let e1), .error(let e2)):
-            #if os(Linux)
-            return  "\(e1)" == "\(e2)"
-            #else
-            let error1 = e1 as NSError
-            let error2 = e2 as NSError
-            
-            return error1.domain == error2.domain
-                && error1.code == error2.code
-                && "\(e1)" == "\(e2)"
-            #endif
-        case (.next(let v1), .next(let v2)): return v1 == v2
-        default: return false
-        }
-    }
+public func == <Element: Equatable>(lhs: Event<Element>, rhs: Event<Element>) -> Bool {
+    return equals(lhs: lhs, rhs: rhs)
 }
 
 /// Compares two events. They are equal if they are both the same member of `SingleEvent` enumeration.
 ///
 /// In case `Error` events are being compared, they are equal in case their `NSError` representations are equal (domain and code)
 /// and their string representations are equal.
-extension SingleEvent: Equatable where Element: Equatable {
-    public static func == (lhs: SingleEvent<Element>, rhs: SingleEvent<Element>) -> Bool {
-        switch (lhs, rhs) {
-        case (.error(let e1), .error(let e2)):
-            #if os(Linux)
-            return  "\(e1)" == "\(e2)"
-            #else
-            let error1 = e1 as NSError
-            let error2 = e2 as NSError
-
-            return error1.domain == error2.domain
-                && error1.code == error2.code
-                && "\(e1)" == "\(e2)"
-            #endif
-        case (.success(let v1), .success(let v2)): return v1 == v2
-        default: return false
-        }
-    }
+public func == <Element: Equatable>(lhs: SingleEvent<Element>, rhs: SingleEvent<Element>) -> Bool {
+    return equals(lhs: lhs, rhs: rhs)
 }
 
 /// Compares two events. They are equal if they are both the same member of `MaybeEvent` enumeration.
 ///
 /// In case `Error` events are being compared, they are equal in case their `NSError` representations are equal (domain and code)
 /// and their string representations are equal.
-extension MaybeEvent: Equatable where Element: Equatable {
-    public static func == (lhs: MaybeEvent<Element>, rhs: MaybeEvent<Element>) -> Bool {
-        switch (lhs, rhs) {
-        case (.completed, .completed): return true
-        case (.error(let e1), .error(let e2)):
-            #if os(Linux)
-            return  "\(e1)" == "\(e2)"
-            #else
-            let error1 = e1 as NSError
-            let error2 = e2 as NSError
-
-            return error1.domain == error2.domain
-                && error1.code == error2.code
-                && "\(e1)" == "\(e2)"
-            #endif
-        case (.success(let v1), .success(let v2)): return v1 == v2
-        default: return false
-        }
-    }
+public func == <Element: Equatable>(lhs: MaybeEvent<Element>, rhs: MaybeEvent<Element>) -> Bool {
+    return equals(lhs: lhs, rhs: rhs)
 }
 
 /// Compares two `CompletableEvent` events.
@@ -107,3 +112,23 @@ extension CompletableEvent: Equatable {
         }
     }
 }
+
+#if swift(>=4.1)
+extension Event: Equatable where Element: Equatable {
+    public static func == (lhs: Event<Element>, rhs: Event<Element>) -> Bool {
+        return equals(lhs: lhs, rhs: rhs)
+    }
+}
+
+extension SingleEvent: Equatable where Element: Equatable {
+    public static func == (lhs: SingleEvent<Element>, rhs: SingleEvent<Element>) -> Bool {
+        return equals(lhs: lhs, rhs: rhs)
+    }
+}
+
+extension MaybeEvent: Equatable where Element: Equatable {
+    public static func == (lhs: MaybeEvent<Element>, rhs: MaybeEvent<Element>) -> Bool {
+        return equals(lhs: lhs, rhs: rhs)
+    }
+}
+#endif
