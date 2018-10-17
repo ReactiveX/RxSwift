4c4887fe900ea2e210108f9f558b8ce2e3f20802
Bei Li <kylin.roc@me.com>
2018-10-17 23:47:04 +0800

    Update UISwitch+Rx docs

--- RxCocoa/iOS/UISwitch+Rx.swift
+++ RxCocoa/iOS/UISwitch+Rx.swift
@@ -17,14 +17,12 @@ extension Reactive where Base: UISwitch {
     public var isOn: ControlProperty<Bool> {
         return value
     }
-    
-    /**
-    Reactive wrapper for `isOn` property.
-    
-    **⚠️ Versions prior to iOS 10.2 were leaking `UISwitch`s, so on those versions
-     underlying observable sequence won't complete when nothing holds a strong reference
-     to UISwitch.⚠️**
-    */
+
+    /// Reactive wrapper for `isOn` property.
+    ///
+    /// ⚠️ Versions prior to iOS 10.2 were leaking `UISwitch`'s, so on those versions
+    /// underlying observable sequence won't complete when nothing holds a strong reference
+    /// to `UISwitch`.
     public var value: ControlProperty<Bool> {
         return base.rx.controlPropertyWithDefaultEvents(
             getter: { uiSwitch in
@@ -38,4 +36,3 @@ extension Reactive where Base: UISwitch {
 }
 
 #endif
-
