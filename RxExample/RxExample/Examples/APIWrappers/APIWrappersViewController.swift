e5b58504800961c0ac807a89ae13ef631b7ca20c
GeorgeWu <wuzheng@zerozero.cn>
2018-11-08 16:04:50 +0800

    Remove duplicated subscription in example

--- RxExample/RxExample/Examples/APIWrappers/APIWrappersViewController.swift
+++ RxExample/RxExample/Examples/APIWrappers/APIWrappersViewController.swift
@@ -152,12 +152,6 @@ class APIWrappersViewController: ViewController {
                 })
                 .disposed(by: disposeBag)
 
-            textValue.asObservable()
-                .subscribe(onNext: { [weak self] x in
-                    self?.debug("UITextField text \(x)")
-                })
-                .disposed(by: disposeBag)
-
             let attributedTextValue = BehaviorRelay<NSAttributedString?>(value: NSAttributedString(string: ""))
             _ = textField2.rx.attributedText <-> attributedTextValue
 
