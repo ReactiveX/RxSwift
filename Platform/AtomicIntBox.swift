faefc0f9ac57553c7004a9e2dd819efdb4553be0
Luciano Almeida <passos.luciano@outlook.com>
2019-01-27 17:22:04 -0200

    Removing inline attributes.

--- Platform/AtomicIntBox.swift
+++ Platform/AtomicIntBox.swift
@@ -10,40 +10,34 @@ import RxAtomic
 
 typealias RxAtomicInt = RxAtomic.AtomicInt
 public final class AtomicIntBox {
-    @usableFromInline
+
     var value: RxAtomicInt
     
-    @inlinable
     public init() {
         self.value = RxAtomic.AtomicInt()
         RxAtomicInt.initialize(&self.value, 0)
     }
     
-    @inlinable
     public init(_ initialValue: Int32) {
         self.value = RxAtomic.AtomicInt()
         RxAtomicInt.initialize(&self.value, initialValue)
     }
     
     @discardableResult
-    @inlinable
     public func add(_ value: Int32) -> Int32 {
         return RxAtomicInt.add(&self.value, value)
     }
     
     @discardableResult
-    @inlinable
     public func sub(_ value: Int32) -> Int32 {
         return RxAtomicInt.sub(&self.value, value)
     }
     
     @discardableResult
-    @inlinable
     public func fetchOr(_ mask: Int32) -> Int32 {
         return RxAtomicInt.fetchOr(&self.value, mask)
     }
     
-    @inlinable
     public func load() -> Int32 {
         return RxAtomicInt.load(&self.value)
     }
