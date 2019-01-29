e9d711b0ec70e6a63bb736dd889d1518697e7a77
Krunoslav Zaher <krunoslav.zaher@gmail.com>
2019-01-30 00:12:37 +0100

    Fix CI.

--- scripts/validate-podspec.sh
+++ scripts/validate-podspec.sh
@@ -3,7 +3,7 @@
 
 set -e
 
-BRANCH=$(git rev-parse --abbrev-ref HEAD)
+BRANCH=$(git rev-parse HEAD)
 ESCAPED_SOURCE=$(pwd | sed -E "s/\//\\\\\//g")
 
 function cleanup {
@@ -15,39 +15,53 @@ function cleanup {
 
 trap cleanup EXIT
 
+if [[ ! -z "${TRAVIS}" ]]; then
+    gem install cocoapods --pre --no-rdoc --no-ri --no-document --quiet;
+    pod repo update;
+fi;
+
 VERSION=`cat RxSwift.podspec | grep -E "s.version\s+=" | cut -d '"' -f 2`
-TARGETS=(RxTest RxCocoa RxBlocking RxAtomic RxSwift)
 ROOTS=(2/e/c 3/c/1 8/5/5 f/7/9 a/b/1)
+ALL_TARGETS=(RxTest RxCocoa RxBlocking RxAtomic RxSwift)
+
+if [ ! -z "$TARGET" ]
+then
+    TARGETS=("$TARGET")
+else
+    TARGETS="${ALL_TARGETS}"
+fi
+
+if [ ! -z "$SWIFT_VERSION" ]
+then
+    SWIFT_VERSION="--swift-version=${SWIFT_VERSION}"
+fi
+
+SOURCE_DIR=`pwd`
 
 pushd ~/.cocoapods/repos/master/Specs
-for TARGET in ${TARGETS[@]}
-do
-  mkdir -p ${TARGET}/${VERSION}
-done
-popd
-
-for TARGET in ${TARGETS[@]}
-do
-
-
     for ROOT in ${ROOTS[@]} ; do
-        mkdir -p ~/.cocoapods/repos/master/Specs/${ROOT}/${TARGET}/${VERSION}
-        rm       ~/.cocoapods/repos/master/Specs/${ROOT}/${TARGET}/${VERSION}/* || echo
-        cat $TARGET.podspec |
-        sed -E "s/s.source [^\}]+\}/s.source           = { :git => '${ESCAPED_SOURCE}', :branch => \'${BRANCH}\' }/" > ~/.cocoapods/repos/master/Specs/${ROOT}/${TARGET}/${VERSION}/${TARGET}.podspec
-    done
+        for TARGET in ${ALL_TARGETS[@]}
+        do
+            if [ ! -d "${ROOT}/${TARGET}" ]
+            then
+                continue
+            fi
 
-done
+            mkdir -p ${ROOT}/${TARGET}/${VERSION}
+            rm ${ROOT}/${TARGET}/${VERSION}/* || echo
+            cat "${SOURCE_DIR}/$TARGET.podspec" |
+            sed -E "s/s.source [^\}]+\}/s.source           = { :git => 'file:\/\/${ESCAPED_SOURCE}' }/" > ${ROOT}/${TARGET}/${VERSION}/${TARGET}.podspec
+        done
+    done
+popd
 
 function validate() {
     local PODSPEC=$1
 
-    pod lib lint $PODSPEC --verbose --no-clean --allow-warnings
+    pod lib lint $PODSPEC --verbose --no-clean --allow-warnings "${SWIFT_VERSION}"
 }
 
 for TARGET in ${TARGETS[@]}
 do
-
-validate ${TARGET}.podspec
-
+    validate ${TARGET}.podspec
 done
