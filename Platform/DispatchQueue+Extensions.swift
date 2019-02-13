5a66602312224936a2b18028ecf9e5baacfce14d
Richard Yu <richard.yu@loblaw.ca>
2019-02-13 11:27:22 -0500

    Fixing check to see if queue is on main thread.

--- Platform/DispatchQueue+Extensions.swift
+++ Platform/DispatchQueue+Extensions.swift
@@ -9,13 +9,7 @@
 import Dispatch
 
 extension DispatchQueue {
-    private static var token: DispatchSpecificKey<()> = {
-        let key = DispatchSpecificKey<()>()
-        DispatchQueue.main.setSpecific(key: key, value: ())
-        return key
-    }()
-
     static var isMain: Bool {
-        return DispatchQueue.getSpecific(key: token) != nil
+        return Thread.isMain
     }
 }
