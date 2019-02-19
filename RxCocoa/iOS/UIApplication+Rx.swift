41e39df22df9788095e8fcf784aeb001b4916eae
vkcldhkd <vkcldhkd@gmail.com>
2019-02-19 14:36:20 +0900

    Adding isIgnoringInteractionEvents in UIApplication+Rx

--- RxCocoa/iOS/UIApplication+Rx.swift
+++ RxCocoa/iOS/UIApplication+Rx.swift
@@ -8,17 +8,24 @@
 
 #if os(iOS)
 
-    import UIKit
-    import RxSwift
+import UIKit
+import RxSwift
 
-    extension Reactive where Base: UIApplication {
-        
-        /// Bindable sink for `networkActivityIndicatorVisible`.
-        public var isNetworkActivityIndicatorVisible: Binder<Bool> {
-            return Binder(self.base) { application, active in
-                application.isNetworkActivityIndicatorVisible = active
-            }
+extension Reactive where Base: UIApplication {
+    
+    /// Bindable sink for `networkActivityIndicatorVisible`.
+    public var isNetworkActivityIndicatorVisible: Binder<Bool> {
+        return Binder(self.base) { application, active in
+            application.isNetworkActivityIndicatorVisible = active
         }
     }
+    
+    /// Bindable sink for `isIgnoringInteractionEvents`.
+    public var isIgnoringInteractionEvents: Binder<Bool> {
+        return Binder(self.base, binding: { (application, active) in
+            active ? application.beginIgnoringInteractionEvents() : application.endIgnoringInteractionEvents()
+        })
+    }
+}
 #endif
 
