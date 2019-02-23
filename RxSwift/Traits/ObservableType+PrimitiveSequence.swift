63c0a93feec63ed7dba0a34d3e122ea475bca8b6
Alexey Potemkin <apotemkin@plazius.ru>
2019-02-23 09:24:23 +0300

    Return 'first' operator

--- RxSwift/Traits/ObservableType+PrimitiveSequence.swift
+++ RxSwift/Traits/ObservableType+PrimitiveSequence.swift
@@ -18,6 +18,18 @@ extension ObservableType {
     public func asSingle() -> Single<E> {
         return PrimitiveSequence(raw: AsSingle(source: self.asObservable()))
     }
+    
+    /**
+     The `first` operator emits only the very first item emitted by this Observable,
+     or nil if this Observable completes without emitting anything.
+     
+     - seealso: [single operator on reactivex.io](http://reactivex.io/documentation/operators/first.html)
+     
+     - returns: An observable sequence that emits a single element or nil if the source observable sequence completes without emitting any items.
+     */
+    public func first() -> Single<E?> {
+        return PrimitiveSequence(raw: First(source: self.asObservable()))
+    }
 
     /**
      The `asMaybe` operator throws a `RxError.moreThanOneElement`
