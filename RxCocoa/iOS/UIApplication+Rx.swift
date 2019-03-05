35ebd4a555b5a3f9ea05469c3fcca0253364ccc9
Siarhei <lsn21@ya.ru>
2019-03-05 17:31:03 +0300

    #1 Вспомогательные методы для UIViewController и UIAppDelegate

--- RxCocoa/iOS/UIApplication+Rx.swift
+++ RxCocoa/iOS/UIApplication+Rx.swift
@@ -20,5 +20,38 @@
             }
         }
     }
+
+    extension Reactive where Base: UIApplication {
+        
+        public static var willResignActive: ControlEvent<()> {
+            let source = NotificationCenter.default.rx.notification(UIApplication.willResignActiveNotification).map { _ in return () }
+            
+            return ControlEvent(events: source)
+        }
+        
+        public static var didEnterBackground: ControlEvent<()> {
+            let source = NotificationCenter.default.rx.notification(UIApplication.didEnterBackgroundNotification).map { _ in return () }
+            
+            return ControlEvent(events: source)
+        }
+        
+        public static var willEnterForeground: ControlEvent<()> {
+            let source = NotificationCenter.default.rx.notification(UIApplication.willEnterForegroundNotification).map { _ in return () }
+            
+            return ControlEvent(events: source)
+        }
+        
+        public static var didBecomeActive: ControlEvent<()> {
+            let source = NotificationCenter.default.rx.notification(UIApplication.didBecomeActiveNotification).map { _ in return () }
+            
+            return ControlEvent(events: source)
+        }
+        
+        public static var willTerminate: ControlEvent<()> {
+            let source = NotificationCenter.default.rx.notification(UIApplication.willTerminateNotification).map { _ in return () }
+            
+            return ControlEvent(events: source)
+        }
+    }
 #endif
 
--- RxCocoa/iOS/UIViewController+Rx.swift
+++ RxCocoa/iOS/UIViewController+Rx.swift
@@ -21,4 +21,52 @@
         }
     
     }
+
+    extension Reactive where Base: UIViewController {
+        public var loadView: ControlEvent<()> {
+            return controlEventForVoidReturningSelector(#selector(Base.loadView))
+        }
+        
+        public var loadViewIfNeeded: ControlEvent<()> {
+            return controlEventForVoidReturningSelector(#selector(Base.loadViewIfNeeded))
+        }
+        
+        public var viewDidLoad: ControlEvent<()> {
+            return controlEventForVoidReturningSelector(#selector(Base.viewDidLoad))
+        }
+        
+        public var viewWillAppear: ControlEvent<Bool> {
+            return controlEventForBoolReturningSelector(#selector(Base.viewWillAppear))
+        }
+        
+        public var viewWillLayoutSubviews: ControlEvent<()> {
+            return controlEventForVoidReturningSelector(#selector(Base.viewWillLayoutSubviews))
+        }
+        
+        public var viewDidLayoutSubviews: ControlEvent<()> {
+            return controlEventForVoidReturningSelector(#selector(Base.viewDidLayoutSubviews))
+        }
+        
+        public var viewDidAppear: ControlEvent<Bool> {
+            return controlEventForBoolReturningSelector(#selector(Base.viewDidAppear))
+        }
+        
+        public var viewWillDisappear: ControlEvent<Bool> {
+            return controlEventForBoolReturningSelector(#selector(Base.viewWillDisappear))
+        }
+        
+        public var viewDidDisappear: ControlEvent<Bool> {
+            return controlEventForBoolReturningSelector(#selector(Base.viewDidDisappear))
+        }
+        
+        private func controlEventForVoidReturningSelector(_ sel: Selector) -> ControlEvent<()> {
+            let source = self.methodInvoked(sel).map { _ in return () }
+            return ControlEvent(events: source)
+        }
+        
+        private func controlEventForBoolReturningSelector(_ sel: Selector) -> ControlEvent<Bool> {
+            let source = self.methodInvoked(sel).map { args in return (args.first as? Bool) == true }
+            return ControlEvent(events: source)
+        }
+    }
 #endif
