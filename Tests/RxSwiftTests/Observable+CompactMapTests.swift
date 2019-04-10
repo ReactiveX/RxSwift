d9e54d354068b07765478f98a7aaef09a3a798b3
Michael Long <mlong@clientresourcesinc.com>
2019-04-10 14:11:26 -0500

    Updated tests

--- Tests/RxSwiftTests/Observable+CompactMapTests.swift
+++ Tests/RxSwiftTests/Observable+CompactMapTests.swift
@@ -18,7 +18,8 @@ class ObservableCompactMapTest : RxTest {
 }
 
 extension ObservableCompactMapTest {
-    func test_compactMapComplete() {
+
+    func test_compactMapNilFromClosure() {
         let scheduler = TestScheduler(initialClock: 0)
         
         var invoked = 0
@@ -63,91 +64,43 @@ extension ObservableCompactMapTest {
         XCTAssertEqual(9, invoked)
     }
     
-    func test_compactMapValues() {
+    func test_compactMapNilFromElement() {
         let scheduler = TestScheduler(initialClock: 0)
-        
+
         var invoked = 0
-        
-        let xs = scheduler.createHotObservable([
+
+        let xs: TestableObservable<Int?> = scheduler.createHotObservable([
             .next(110, 1),
             .next(180, 2),
             .next(230, 3),
-            .next(270, 4),
+            .next(270, nil),
             .next(340, 5),
-            .next(380, 6),
-            .next(390, 7),
-            .next(450, 8),
-            .next(470, 9),
-            .next(560, 10),
-            .next(580, 11),
-            .completed(600)
+            .completed(400),
+            .next(410, 7),
+            .error(420, testError),
+            .completed(430)
             ])
-        
+
         let res = scheduler.start { () -> Observable<Int> in
             return xs.compactMap { num in
                 invoked += 1
                 return num
             }
         }
-        
+
         XCTAssertEqual(res.events, [
             .next(230, 3),
-            .next(270, 4),
             .next(340, 5),
-            .next(380, 6),
-            .next(390, 7),
-            .next(450, 8),
-            .next(470, 9),
-            .next(560, 10),
-            .next(580, 11),
-            .completed(600)
+            .completed(400),
             ])
-        
+
         XCTAssertEqual(xs.subscriptions, [
-            Subscription(200, 600)
+            Subscription(200, 400)
             ])
-        
-        XCTAssertEqual(9, invoked)
+
+        XCTAssertEqual(3, invoked)
     }
-   
-    func test_compactMapNil() {
-        let scheduler = TestScheduler(initialClock: 0)
-        
-        var invoked = 0
-        
-        let xs = scheduler.createHotObservable([
-            .next(110, 1),
-            .next(180, 2),
-            .next(230, 3),
-            .next(270, 4),
-            .next(340, 5),
-            .next(380, 6),
-            .next(390, 7),
-            .next(450, 8),
-            .next(470, 9),
-            .next(560, 10),
-            .next(580, 11),
-            .completed(600)
-            ])
-        
-        let res = scheduler.start { () -> Observable<Int> in
-            return xs.compactMap { _ in
-                invoked += 1
-                return nil
-            }
-        }
-        
-        XCTAssertEqual(res.events, [
-            .completed(600)
-            ])
-        
-        XCTAssertEqual(xs.subscriptions, [
-            Subscription(200, 600)
-            ])
-        
-        XCTAssertEqual(9, invoked)
-    }
-    
+
     func test_compactMapDisposed() {
         let scheduler = TestScheduler(initialClock: 0)
         
