b19394116a1f4192332a10b49bad1e56b84e3e23
Michael Collins <michael.collins@naked.software>
2019-04-20 16:29:20 -0700

    Update validate-playgrounds.sh
    I revised my previous change to get validate-playgrounds.sh to build
    the RxPlaygrounds dynamic framework before attempting to build the
    playgrounds test application.

--- scripts/validate-playgrounds.sh
+++ scripts/validate-playgrounds.sh
@@ -5,11 +5,12 @@ PLAYGROUND_CONFIGURATIONS=(Release)
 # make sure macOS builds
 for configuration in ${PLAYGROUND_CONFIGURATIONS[@]}
 do
-  for scheme in "RxSwift RxPlaygrounds"
+  for scheme in "RxSwift"
   do
     rx ${scheme} ${configuration} "" build
   done
 
+  rx RxPlaygrounds ${configuration} "" build
   PAGES_PATH=${BUILD_DIRECTORY}/Build/Products/${configuration}/all-playground-pages.swift
   cat Rx.playground/Sources/*.swift Rx.playground/Pages/**/*.swift > ${PAGES_PATH}
   swiftc -v -D NOT_IN_PLAYGROUND -target x86_64-apple-macosx10.10 -F ${BUILD_DIRECTORY}/Build/Products/${configuration} -framework RxSwift ${PAGES_PATH}   
