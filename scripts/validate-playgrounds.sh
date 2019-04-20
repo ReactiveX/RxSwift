ae46216492e088217c89f6ac72ad8bdc58ae3edc
Michael Collins <michael.collins@naked.software>
2019-04-20 16:17:10 -0700

    Update validate-playgrounds.sh
    I updated validate-playgrounds.sh to also build the RxPlaygrounds
    framework.

--- scripts/validate-playgrounds.sh
+++ scripts/validate-playgrounds.sh
@@ -3,14 +3,15 @@
 PLAYGROUND_CONFIGURATIONS=(Release)
 
 # make sure macOS builds
-for scheme in "RxSwift"
+for configuration in ${PLAYGROUND_CONFIGURATIONS[@]}
 do
-  for configuration in ${PLAYGROUND_CONFIGURATIONS[@]}
+  for scheme in "RxSwift RxPlaygrounds"
   do
-    PAGES_PATH=${BUILD_DIRECTORY}/Build/Products/${configuration}/all-playground-pages.swift
     rx ${scheme} ${configuration} "" build
-    cat Rx.playground/Sources/*.swift Rx.playground/Pages/**/*.swift > ${PAGES_PATH}
-    swiftc -v -D NOT_IN_PLAYGROUND -target x86_64-apple-macosx10.10 -F ${BUILD_DIRECTORY}/Build/Products/${configuration} -framework RxSwift ${PAGES_PATH}   
-    ./all-playground-pages
   done
+
+  PAGES_PATH=${BUILD_DIRECTORY}/Build/Products/${configuration}/all-playground-pages.swift
+  cat Rx.playground/Sources/*.swift Rx.playground/Pages/**/*.swift > ${PAGES_PATH}
+  swiftc -v -D NOT_IN_PLAYGROUND -target x86_64-apple-macosx10.10 -F ${BUILD_DIRECTORY}/Build/Products/${configuration} -framework RxSwift ${PAGES_PATH}   
+  ./all-playground-pages
 done
