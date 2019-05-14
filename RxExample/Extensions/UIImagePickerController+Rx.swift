fb2ff2d7be7780045a3f89107f1e5261dd0a7a09
antonnazarov <nazarof.anton@gmail.com>
2019-05-14 20:05:58 +0300

    Change return type of didFinishPickingMediaWithInfo

--- RxExample/Extensions/UIImagePickerController+Rx.swift
+++ RxExample/Extensions/UIImagePickerController+Rx.swift
@@ -18,11 +18,11 @@
         /**
          Reactive wrapper for `delegate` message.
          */
-        public var didFinishPickingMediaWithInfo: Observable<[String : AnyObject]> {
+        public var didFinishPickingMediaWithInfo: Observable<[UIImagePickerController.InfoKey : AnyObject]> {
             return delegate
                 .methodInvoked(#selector(UIImagePickerControllerDelegate.imagePickerController(_:didFinishPickingMediaWithInfo:)))
                 .map({ (a) in
-                    return try castOrThrow(Dictionary<String, AnyObject>.self, a[1])
+                    return try castOrThrow(Dictionary<UIImagePickerController.InfoKey, AnyObject>.self, a[1])
                 })
         }
 
