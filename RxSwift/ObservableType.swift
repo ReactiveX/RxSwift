2228b82a11a79bebbf7235296a2643d4ff530cce
yuriferretti <yuriferretti@gmail.com>
2019-09-23 21:16:45 -0300

    Update RxSwift/ObservableType.swift
    Co-Authored-By: Shai Mishali <freak4pc@gmail.com>

--- RxSwift/ObservableType.swift
+++ RxSwift/ObservableType.swift
@@ -52,7 +52,7 @@ Allows easier mapping by exposing dynamic member keypaths
 So Observable.just("string").map { $0.count } can rewritten as Observable.just("string").count
 */
 extension ObservableType {
-    public subscript<U>(dynamicMember keyPath: KeyPath<Element, U>) -> Observable<U> {
+    public subscript<Member>(dynamicMember keyPath: KeyPath<Element, Member>) -> Observable<Member> {
         return self.map { $0[keyPath: keyPath] }
     }
 }
