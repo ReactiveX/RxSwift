bc9b693cea662391cbf31be3a0ae2ca3889bb1a1
freak4pc <freak4pc@gmail.com>
2019-10-12 23:07:02 +0300

    Fix FoundationNetworking imports for Linux builds
    See https://github.com/apple/swift-corelibs-foundation/blob/a264bfd40af8412161cdc05be11c1cf35f6a56f1/Docs/ReleaseNotes_Swift5.md#dependency-management

--- RxCocoa/Foundation/URLSession+Rx.swift
+++ RxCocoa/Foundation/URLSession+Rx.swift
@@ -7,17 +7,25 @@
 //
 
 import struct Foundation.URL
-import struct Foundation.URLRequest
 import struct Foundation.Data
 import struct Foundation.Date
 import struct Foundation.TimeInterval
-import class Foundation.HTTPURLResponse
-import class Foundation.URLSession
-import class Foundation.URLResponse
 import class Foundation.JSONSerialization
 import class Foundation.NSError
 import var Foundation.NSURLErrorCancelled
 import var Foundation.NSURLErrorDomain
+
+#if canImport(FoundationNetworking)
+import struct FoundationNetworking.URLRequest
+import class FoundationNetworking.HTTPURLResponse
+import class FoundationNetworking.URLSession
+import class FoundationNetworking.URLResponse
+#else
+import struct Foundation.URLRequest
+import class Foundation.HTTPURLResponse
+import class Foundation.URLSession
+import class Foundation.URLResponse
+#endif
 
 #if os(Linux)
     // don't know why
