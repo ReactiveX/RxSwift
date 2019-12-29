e9405fd79e4af6c9fc7955397f025fe2cb317c53
freak4pc <freak4pc@gmail.com>
2019-12-29 21:18:23 +0200

    Remove all deprecations

--- RxSwift/ObservableConvertibleType.swift
+++ RxSwift/ObservableConvertibleType.swift
@@ -11,9 +11,6 @@ public protocol ObservableConvertibleType {
     /// Type of elements in sequence.
     associatedtype Element
 
-    @available(*, deprecated, renamed: "Element")
-    typealias E = Element
-
     /// Converts `self` to `Observable` sequence.
     ///
     /// - returns: Observable sequence that represents `self`.
