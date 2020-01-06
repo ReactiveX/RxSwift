ed9471a83cb6ac7b3294bd37faad7a3a9c1d0f25
Ayal Spitz <github@blackgecko.com>
2020-01-06 07:01:18 -0500

    Updated code in response to PR comments

--- RxSwift/Observables/Sample.swift
+++ RxSwift/Observables/Sample.swift
@@ -13,13 +13,13 @@ extension ObservableType {
 
      Upon each sampling tick, the latest element (if any) in the source sequence during the last sampling interval is sent to the resulting sequence.
 
-     **In case there were no new elements between sampler ticks, if a default value has been set then it is sent
+     **In case there were no new elements between sampler ticks, you may provide a default value to be emitted, instead
        to the resulting sequence otherwise no element is sent.**
 
      - seealso: [sample operator on reactivex.io](http://reactivex.io/documentation/operators/sample.html)
 
      - parameter sampler: Sampling tick sequence.
-                 defaultValue: a value to return if there are new new elements between sampler ticks
+     - parameter defaultValue: a value to return if there are no new elements between sampler ticks
      - returns: Sampled observable sequence.
      */
     public func sample<Source: ObservableType>(_ sampler: Source, defaultValue: Element? = nil)
@@ -53,11 +53,9 @@ final private class SamplerSink<Observer: ObserverType, SampleType>
     func synchronized_on(_ event: Event<Element>) {
         switch event {
         case .next, .completed:
-            if let element = parent.element {
+            if let element = parent.element ?? self.parent.defaultValue{
                 self.parent.element = nil
                 self.parent.forwardOn(.next(element))
-            } else if let element = self.parent.parent.defaultValue {
-               self.parent.forwardOn(.next(element))
             }
 
             if self.parent.atEnd {
@@ -80,6 +78,7 @@ final private class SampleSequenceSink<Observer: ObserverType, SampleType>
     typealias Parent = Sample<Element, SampleType>
     
     fileprivate let parent: Parent
+    fileprivate let defaultValue: Element?
 
     let lock = RecursiveLock()
     
@@ -89,8 +88,9 @@ final private class SampleSequenceSink<Observer: ObserverType, SampleType>
     
     private let sourceSubscription = SingleAssignmentDisposable()
     
-    init(parent: Parent, observer: Observer, cancel: Cancelable) {
+    init(parent: Parent, observer: Observer, cancel: Cancelable, defaultValue: Element? = nil) {
         self.parent = parent
+        self.defaultValue = defaultValue
         super.init(observer: observer, cancel: cancel)
     }
     
@@ -132,7 +132,7 @@ final private class Sample<Element, SampleType>: Producer<Element> {
     }
     
     override func run<Observer: ObserverType>(_ observer: Observer, cancel: Cancelable) -> (sink: Disposable, subscription: Disposable) where Observer.Element == Element {
-        let sink = SampleSequenceSink(parent: self, observer: observer, cancel: cancel)
+        let sink = SampleSequenceSink(parent: self, observer: observer, cancel: cancel, defaultValue: self.defaultValue)
         let subscription = sink.run()
         return (sink: sink, subscription: subscription)
     }
