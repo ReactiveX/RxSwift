21abb37065a92b9a6ed1dc52df0066c08540ca55
Ayal Spitz <github@blackgecko.com>
2020-01-09 02:06:12 -0500

    Extended Sample operation to support a default value (#1457)

--- RxSwift/Observables/Sample.swift
+++ RxSwift/Observables/Sample.swift
@@ -13,16 +13,18 @@ extension ObservableType {
 
      Upon each sampling tick, the latest element (if any) in the source sequence during the last sampling interval is sent to the resulting sequence.
 
-     **In case there were no new elements between sampler ticks, no element is sent to the resulting sequence.**
+     **In case there were no new elements between sampler ticks, you may provide a default value to be emitted, instead
+       to the resulting sequence otherwise no element is sent.**
 
      - seealso: [sample operator on reactivex.io](http://reactivex.io/documentation/operators/sample.html)
 
      - parameter sampler: Sampling tick sequence.
+     - parameter defaultValue: a value to return if there are no new elements between sampler ticks
      - returns: Sampled observable sequence.
      */
-    public func sample<Source: ObservableType>(_ sampler: Source)
+    public func sample<Source: ObservableType>(_ sampler: Source, defaultValue: Element? = nil)
         -> Observable<Element> {
-            return Sample(source: self.asObservable(), sampler: sampler.asObservable())
+            return Sample(source: self.asObservable(), sampler: sampler.asObservable(), defaultValue: defaultValue)
     }
 }
 
@@ -51,7 +53,7 @@ final private class SamplerSink<Observer: ObserverType, SampleType>
     func synchronized_on(_ event: Event<Element>) {
         switch event {
         case .next, .completed:
-            if let element = parent.element {
+            if let element = parent.element ?? self.parent.defaultValue {
                 self.parent.element = nil
                 self.parent.forwardOn(.next(element))
             }
@@ -75,7 +77,8 @@ final private class SampleSequenceSink<Observer: ObserverType, SampleType>
     typealias Element = Observer.Element 
     typealias Parent = Sample<Element, SampleType>
     
-    private let parent: Parent
+    fileprivate let parent: Parent
+    fileprivate let defaultValue: Element?
 
     let lock = RecursiveLock()
     
@@ -85,8 +88,9 @@ final private class SampleSequenceSink<Observer: ObserverType, SampleType>
     
     private let sourceSubscription = SingleAssignmentDisposable()
     
-    init(parent: Parent, observer: Observer, cancel: Cancelable) {
+    init(parent: Parent, observer: Observer, cancel: Cancelable, defaultValue: Element? = nil) {
         self.parent = parent
+        self.defaultValue = defaultValue
         super.init(observer: observer, cancel: cancel)
     }
     
@@ -119,14 +123,16 @@ final private class SampleSequenceSink<Observer: ObserverType, SampleType>
 final private class Sample<Element, SampleType>: Producer<Element> {
     fileprivate let source: Observable<Element>
     fileprivate let sampler: Observable<SampleType>
-
-    init(source: Observable<Element>, sampler: Observable<SampleType>) {
+    fileprivate let defaultValue: Element?
+    
+    init(source: Observable<Element>, sampler: Observable<SampleType>, defaultValue: Element? = nil) {
         self.source = source
         self.sampler = sampler
+        self.defaultValue = defaultValue
     }
     
     override func run<Observer: ObserverType>(_ observer: Observer, cancel: Cancelable) -> (sink: Disposable, subscription: Disposable) where Observer.Element == Element {
-        let sink = SampleSequenceSink(parent: self, observer: observer, cancel: cancel)
+        let sink = SampleSequenceSink(parent: self, observer: observer, cancel: cancel, defaultValue: self.defaultValue)
         let subscription = sink.run()
         return (sink: sink, subscription: subscription)
     }
