4c8ee3a036643f89afb112578edfe1ea913757f4
Adam Sharp <adam@sharplet.me>
2020-07-02 14:25:16 -0400

    Use SwiftPM default minimum deployment targets
    Xcode 12 raises the minimum known iOS deployment target from iOS 8.0 to
    iOS 9.0, resulting in a warning when using RxSwift via SwiftPM. Instead
    of explicitly specifying minimum deployment targets, we can use
    SwiftPM's default values which will stay up-to-date whenever the
    toolchain in use changes the valid deployment target range.

--- Package.swift
+++ Package.swift
@@ -42,9 +42,6 @@ extension Target {
 
 let package = Package(
   name: "RxSwift",
-  platforms: [
-    .macOS(.v10_10), .iOS(.v8), .tvOS(.v9), .watchOS(.v3)
-  ],
   products: ([
     [
       .library(name: "RxSwift", targets: ["RxSwift"]),
