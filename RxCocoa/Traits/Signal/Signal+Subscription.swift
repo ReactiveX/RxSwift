d28fb617e8484ff629a8cb9fd903b20a70387520
freak4pc <freak4pc@gmail.com>
2021-01-30 23:02:54 +0200

    subscribe with object for Driver/Signal

--- RxCocoa/Traits/Signal/Signal+Subscription.swift
+++ RxCocoa/Traits/Signal/Signal+Subscription.swift
@@ -112,12 +112,17 @@ extension SharedSequenceConvertibleType where SharingStrategy == SignalSharingSt
             relays.forEach { $0.accept(e) }
         })
     }
-
+    
     /**
      Subscribes an element handler, a completion handler and disposed handler to an observable sequence.
 
+     Also, take in an object and provide an unretained, safe to use (i.e. not implicitly unwrapped), reference to it along with the events emitted by the sequence.
+
      Error callback is not exposed because `Signal` can't error out.
 
+     - Note: If `object` can't be retained, none of the other closures will be invoked.
+     
+     - parameter object: The object to provide an unretained reference on.
      - parameter onNext: Action to invoke for each element in the observable sequence.
      - parameter onCompleted: Action to invoke upon graceful termination of the observable sequence.
      gracefully completed, errored, or if the generation is canceled by disposing subscription)
@@ -125,7 +130,37 @@ extension SharedSequenceConvertibleType where SharingStrategy == SignalSharingSt
      gracefully completed, errored, or if the generation is canceled by disposing subscription)
      - returns: Subscription object used to unsubscribe from the observable sequence.
      */
-    public func emit(onNext: ((Element) -> Void)? = nil, onCompleted: (() -> Void)? = nil, onDisposed: (() -> Void)? = nil) -> Disposable {
+    public func emit<Object: AnyObject>(
+        with object: Object,
+        onNext: ((Object, Element) -> Void)? = nil,
+        onCompleted: ((Object) -> Void)? = nil,
+        onDisposed: ((Object) -> Void)? = nil
+    ) -> Disposable {
+        self.asObservable().subscribe(
+            with: object,
+            onNext: onNext,
+            onCompleted: onCompleted,
+            onDisposed: onDisposed
+        )
+    }
+
+    /**
+     Subscribes an element handler, a completion handler and disposed handler to an observable sequence.
+
+     Error callback is not exposed because `Signal` can't error out.
+     
+     - parameter onNext: Action to invoke for each element in the observable sequence.
+     - parameter onCompleted: Action to invoke upon graceful termination of the observable sequence.
+     gracefully completed, errored, or if the generation is canceled by disposing subscription)
+     - parameter onDisposed: Action to invoke upon any type of termination of sequence (if the sequence has
+     gracefully completed, errored, or if the generation is canceled by disposing subscription)
+     - returns: Subscription object used to unsubscribe from the observable sequence.
+     */
+    public func emit(
+        onNext: ((Element) -> Void)? = nil,
+        onCompleted: (() -> Void)? = nil,
+        onDisposed: (() -> Void)? = nil
+    ) -> Disposable {
         self.asObservable().subscribe(onNext: onNext, onCompleted: onCompleted, onDisposed: onDisposed)
     }
 
