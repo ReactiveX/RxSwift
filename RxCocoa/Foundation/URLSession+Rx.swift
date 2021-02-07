1462d160a07090c5571c76fa4d27f36c756c598f
Daniel Tartaglia <danielt1263@gmail.com>
2021-02-07 10:31:38 -0500

    If URLRequest has httpBody, ensure that it's show in the curl text.

--- RxCocoa/Foundation/URLSession+Rx.swift
+++ RxCocoa/Foundation/URLSession+Rx.swift
@@ -51,7 +51,7 @@ private func convertURLRequestToCurlCommand(_ request: URLRequest) -> String {
     let method = request.httpMethod ?? "GET"
     var returnValue = "curl -X \(method) "
 
-    if let httpBody = request.httpBody, request.httpMethod == "POST" || request.httpMethod == "PUT" {
+    if let httpBody = request.httpBody {
         let maybeBody = String(data: httpBody, encoding: String.Encoding.utf8)
         if let body = maybeBody {
             returnValue += "-d \"\(escapeTerminalString(body))\" "
