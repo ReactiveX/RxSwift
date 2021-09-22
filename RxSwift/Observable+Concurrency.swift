e9ac699757e92889fbc0a8ead74cb23876c5b3ae
Shai Mishali <freak4pc@gmail.com>
2021-09-22 12:14:36 +0300

    [Concurrency] Infallible

--- RxSwift/Observable+Concurrency.swift
+++ RxSwift/Observable+Concurrency.swift
@@ -37,3 +37,28 @@ public extension ObservableConvertibleType {
         }
     }
 }
+
+// MARK: - Infallible
+@available(macOS 12, iOS 15, tvOS 15, watchOS 8, *)
+public extension InfallibleType {
+    /// Allows iterating over the values of an Infallible
+    /// asynchronously via Swift's concurrency features (`async/await`)
+    ///
+    /// A sample usage would look like so:
+    ///
+    /// ```swift
+    /// for await value in observable.values {
+    ///     // Handle emitted values
+    /// }
+    /// ```
+    var values: AsyncStream<Element> {
+        AsyncStream<Element> { continuation in
+            _ = subscribe(
+                onNext: { value in continuation.yield(value) },
+                onCompleted: { continuation.finish() },
+                onDisposed: { continuation.onTermination?(.cancelled) }
+            )
+        }
+    }
+}
+#endif
