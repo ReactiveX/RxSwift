fe2063e5d71c39a868b50b73d533000e8f13f8a8
Shai Mishali <freak4pc@gmail.com>
2021-09-22 12:14:17 +0300

    [Concurrency] Observable

---
+++ RxSwift/Observable+Concurrency.swift
@@ -0,0 +1,39 @@
+//
+//  Observable+Concurrency.swift
+//  RxSwift
+//
+//  Created by Shai Mishali on 22/09/2021.
+//  Copyright © 2021 Krunoslav Zaher. All rights reserved.
+//
+
+#if swift(>=5.5)
+import Foundation
+
+// MARK: - Observable (+ Convertibles)
+@available(macOS 12, iOS 15, tvOS 15, watchOS 8, *)
+public extension ObservableConvertibleType {
+    /// Allows iterating over the values of an Observable
+    /// asynchronously via Swift's concurrency features (`async/await`)
+    ///
+    /// A sample usage would look like so:
+    ///
+    /// ```swift
+    /// do {
+    ///     for try await value in observable.values {
+    ///         // Handle emitted values
+    ///     }
+    /// } catch {
+    ///     // Handle error
+    /// }
+    /// ```
+    var values: AsyncThrowingStream<Element, Error> {
+        AsyncThrowingStream<Element, Error> { continuation in
+            _ = asObservable().subscribe(
+                onNext: { value in continuation.yield(value) },
+                onError: { error in continuation.finish(throwing: error) },
+                onCompleted: { continuation.finish() },
+                onDisposed: { continuation.onTermination?(.cancelled) }
+            )
+        }
+    }
+}
