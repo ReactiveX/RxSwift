53afbd6d3f379b4ec442ebc51304ec81d8564673
Barczi Bálint <balint.barczi@supercharge.io>
2021-10-05 18:37:48 +0200

    chore: add dynamic support to libraries

--- Package.swift
+++ Package.swift
@@ -50,6 +50,11 @@ let package = Package(
       .library(name: "RxRelay", targets: ["RxRelay"]),
       .library(name: "RxBlocking", targets: ["RxBlocking"]),
       .library(name: "RxTest", targets: ["RxTest"]),
+      .library(name: "RxSwift-Dynamic", type: .dynamic, targets: ["RxSwift"]),
+      .library(name: "RxCocoa-Dynamic", type: .dynamic, targets: ["RxCocoa"]),
+      .library(name: "RxRelay-Dynamic", type: .dynamic, targets: ["RxRelay"]),
+      .library(name: "RxBlocking-Dynamic", type: .dynamic, targets: ["RxBlocking"]),
+      .library(name: "RxTest-Dynamic", type: .dynamic, targets: ["RxTest"]),
     ],
     Product.allTests()
   ] as [[Product]]).flatMap { $0 },
