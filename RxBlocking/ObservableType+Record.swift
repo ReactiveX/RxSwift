fc7cd25b27ff1d8138a7671de543f30b99a5eb37
dongyoung.lee <dongyoung.lee@naverz-corp.com>
2022-03-19 08:05:34 +0900

    [RxBlocking] add recorded operator

---
+++ RxBlocking/ObservableType+Record.swift
@@ -0,0 +1,21 @@
+//
+//  ObservableType+Record.swift
+//  
+//
+//  Created by Booung on 2022/03/19.
+//
+
+import RxSwift
+import Foundation
+
+extension ObservableType {
+    /// Converts an `Observable` that records last `N` events
+    ///
+    /// - parameter bufferSize: Number of recent recordable events
+    /// - returns: `Observable<Element>` version of `self`
+    func recorded(_ bufferSize: Int = 10) -> Observable<Element> {
+        let replaySubject = ReplaySubject<Element>.create(bufferSize: bufferSize)
+        _ = self.subscribe(replaySubject)
+        return replaySubject.asObservable()
+    }
+}
