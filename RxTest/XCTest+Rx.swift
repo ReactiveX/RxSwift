7b8a00483da3151a92cd5e586e97e3c3c142c612
Rodrigo Kreutz <8869678+rkreutz@users.noreply.github.com>
2023-05-11 13:43:48 +0100

    RxTest: Comparing failure case in XCTAssertEqual for SingleEvent (#2486)
    * Comparing failure case in XCTAssertEqual for SingleEvent
    
    * Fixing printSequenceDifferences for SingleEvent

--- RxTest/XCTest+Rx.swift
+++ RxTest/XCTest+Rx.swift
@@ -51,8 +51,8 @@ public func XCTAssertEqual<Element: Equatable>(_ lhs: [Event<Element>], _ rhs: [
  - parameter line: The line number on which it appears.
  */
 public func XCTAssertEqual<Element: Equatable>(_ lhs: [SingleEvent<Element>], _ rhs: [SingleEvent<Element>], file: StaticString = #file, line: UInt = #line) {
-    let leftEquatable = lhs.map { AnyEquatable(target: try? $0.get(), comparer: ==) }
-    let rightEquatable = rhs.map { AnyEquatable(target: try? $0.get(), comparer: ==) }
+    let leftEquatable = lhs.map { AnyEquatable(target: $0, comparer: equals) }
+    let rightEquatable = rhs.map { AnyEquatable(target: $0, comparer: equals) }
     #if os(Linux)
         XCTAssertEqual(leftEquatable, rightEquatable)
     #else
@@ -62,7 +62,7 @@ public func XCTAssertEqual<Element: Equatable>(_ lhs: [SingleEvent<Element>], _ 
         return
     }
 
-    printSequenceDifferences(lhs.map { try? $0.get() }, rhs.map { try? $0.get() }, ==)
+    printSequenceDifferences(lhs, rhs, equals)
 }
 
 /**
