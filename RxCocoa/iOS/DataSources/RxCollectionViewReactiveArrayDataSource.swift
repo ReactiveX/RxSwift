574e7522f1de0b787bc4d308321be9bff9e44fd6
daisuke0131 <dai.tachikoma@gmail.com>
2023-07-17 21:38:20 +0900

    patch for xcode14.3.1

--- RxCocoa/iOS/DataSources/RxCollectionViewReactiveArrayDataSource.swift
+++ RxCocoa/iOS/DataSources/RxCollectionViewReactiveArrayDataSource.swift
@@ -42,10 +42,6 @@ class RxCollectionViewReactiveArrayDataSourceSequenceWrapper<S: Sequence>
     : RxCollectionViewReactiveArrayDataSource<S.Iterator.Element>
     , RxCollectionViewDataSourceType {
     typealias Element = S
-
-    override init(cellFactory: @escaping CellFactory) {
-        super.init(cellFactory: cellFactory)
-    }
     
     func collectionView(_ collectionView: UICollectionView, observedEvent: Event<S>) {
         Binder(self) { collectionViewDataSource, sectionModels in
--- RxCocoa/iOS/DataSources/RxTableViewReactiveArrayDataSource.swift
+++ RxCocoa/iOS/DataSources/RxTableViewReactiveArrayDataSource.swift
@@ -43,10 +43,6 @@ class RxTableViewReactiveArrayDataSourceSequenceWrapper<S: Sequence>
     , RxTableViewDataSourceType {
     typealias Element = S
 
-    override init(cellFactory: @escaping CellFactory) {
-        super.init(cellFactory: cellFactory)
-    }
-
     func tableView(_ tableView: UITableView, observedEvent: Event<S>) {
         Binder(self) { tableViewDataSource, sectionModels in
             let sections = Array(sectionModels)
