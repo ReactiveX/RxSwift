f4c79e3e75e5af57759f1c7edc7290362aa91b9d
Benjamin Maer <ben@resplendent.co>
2024-01-04 17:37:17 -0500

    Updated optional map to take a function as a parameter, instead of a closure.
    Co-authored-by: Shai Mishali <freak4pc@gmail.com>

--- RxSwift/Traits/PrimitiveSequence/Completable.swift
+++ RxSwift/Traits/PrimitiveSequence/Completable.swift
@@ -123,7 +123,7 @@ extension PrimitiveSequenceType where Trait == CompletableTrait, Element == Swif
                 let callStack = [String]()
         #endif
 
-        let disposable: Disposable = onDisposed.map({ Disposables.create(with: $0) }) ?? Disposables.create()
+        let disposable: Disposable = onDisposed.map(Disposables.create(with:)) ?? Disposables.create()
 
         let observer: CompletableObserver = { event in
             switch event {
