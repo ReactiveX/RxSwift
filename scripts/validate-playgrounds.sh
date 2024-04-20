90911e6b917697eff9d9628eaaaebfa7c5216319
Shai Mishali <freak4pc@gmail.com>
2024-04-21 00:16:59 +0300

    more

--- scripts/validate-playgrounds.sh
+++ scripts/validate-playgrounds.sh
@@ -8,8 +8,9 @@ do
   for configuration in ${PLAYGROUND_CONFIGURATIONS[@]}
   do
     PAGES_PATH=${BUILD_DIRECTORY}/Build/Products/${configuration}/all-playground-pages.swift
+    echo $PAGES_PATH
     rx ${scheme} ${configuration} "" build
     cat Rx.playground/Sources/*.swift Rx.playground/Pages/**/*.swift > ${PAGES_PATH}
-    swift -v -D NOT_IN_PLAYGROUND -target x86_64-apple-macosx10.10 -F ${BUILD_DIRECTORY}/Build/Products/${configuration} ${PAGES_PATH}   
+    swift -v -D NOT_IN_PLAYGROUND -F ${BUILD_DIRECTORY}/Build/Products/${configuration} ${PAGES_PATH}   
   done
 done
