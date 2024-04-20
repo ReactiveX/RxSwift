b44b19c0913668ebfb7f377eb49317284580811b
Shai Mishali <freak4pc@gmail.com>
2024-04-21 00:31:16 +0300

    vision darwin

--- Platform/Platform.Darwin.swift
+++ Platform/Platform.Darwin.swift
@@ -6,7 +6,7 @@
 //  Copyright © 2015 Krunoslav Zaher. All rights reserved.
 //
 
-#if os(macOS) || os(iOS) || os(tvOS) || os(watchOS) || os(visionOS)
+#if os(macOS) || os(iOS) || os(tvOS) || os(watchOS)
 
     import Darwin
     import Foundation
