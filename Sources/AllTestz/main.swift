bf641100e745cc676c56d6e77ea5ae0c94c28859
Arthur Liu <arthurliu1994@gmail.com>
2026-07-22 10:42:38 -0700

    Fix concurrent share subscriptions

--- Sources/AllTestz/main.swift
+++ Sources/AllTestz/main.swift
@@ -24,6 +24,7 @@ final class AnomaliesTest_ : AnomaliesTest, RxTestCase {
     ("test1323", AnomaliesTest.test1323),
     ("test1344", AnomaliesTest.test1344),
     ("testSeparationBetweenOnAndSubscriptionLocks", AnomaliesTest.testSeparationBetweenOnAndSubscriptionLocks),
+    ("test2718ShareWithoutReplayConnectsOnceForConcurrentFirstSubscriptions", AnomaliesTest.test2718ShareWithoutReplayConnectsOnceForConcurrentFirstSubscriptions),
     ("test2653ShareReplayOneInitialEmissionDeadlock", AnomaliesTest.test2653ShareReplayOneInitialEmissionDeadlock),
     ("test2653ShareReplayMoreInitialEmissionDeadlock", AnomaliesTest.test2653ShareReplayMoreInitialEmissionDeadlock),
     ("test2653ShareReplayOneForeverInitialEmissionDeadlock", AnomaliesTest.test2653ShareReplayOneForeverInitialEmissionDeadlock),
